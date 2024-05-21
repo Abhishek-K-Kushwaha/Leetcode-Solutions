179a254d78722e5b0675df7a1800988500ed64c0
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-05-21 18:24:58 +0530

    Update 0121-best-time-to-buy-and-sell-stock.cpp

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -2,17 +2,11 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int minimum = INT_MAX;
-        int maximum = 0;
         int profit = 0;
-        for (int i = 0; i < prices.size(); i++){
-            if (minimum > prices[i] && i != prices.size()-1){
-                minimum = prices[i];
-                maximum = prices[i];
-            }
-            maximum = max(maximum,prices[i]);
-            profit = max(profit, maximum - minimum);
+        for (int i = 0; i < prices.size(); i++) {
+            minimum = min(minimum, prices[i]);
+            profit = max(profit, prices[i] - minimum);
         }
         return profit;
-
     }
 };
