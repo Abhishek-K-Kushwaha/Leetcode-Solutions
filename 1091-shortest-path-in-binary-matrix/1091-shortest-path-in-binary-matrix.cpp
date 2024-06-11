3adaff93e0cb3ea2c48607b3e4e5eaf339c17ffa
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-06-11 16:25:23 +0530

    Time: 84 ms (22.68%), Space: 44.8 MB (11.26%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n=grid.size(),m=grid[0].size();
-        queue<pair<int,pair<int,int>>> q;
-        if(grid[0][0]==1 || grid[n-1][m-1]==1){return -1;}
-        vector<vector<int>> dist(n,vector<int>(m,1e9));
-        q.push({1,{0,0}});
-        dist[0][0]=1;
-        vector<pair<int,int>> dir={{0,1},{0,-1},{-1,0},{1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
-
-        while(!q.empty()){
-            int dis=q.front().first;
-            auto [i,j]=q.front().second;
+        if (grid[0][0] == 1)
+            return -1;
+        int n = grid.size();
+        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+        queue<pair<int, int>> q;
+        q.push({0, 0});
+        dist[0][0] = 1;
+        while (!q.empty()) {
+            int x = q.front().first;
+            int y = q.front().second;
             q.pop();
-
-            for(auto it:dir){
-                int x=i+it.first,y=j+it.second;
-                if(x>=0 && x<n && y>=0 && y<m && grid[x][y]==0){
-                    if(dis+1 < dist[x][y]){
-                        dist[x][y]=dis+1;
-                        q.push({dist[x][y],{x,y}});
-                    }
+            int d = dist[x][y];
+            vector<int> delx = {0, 0, 1, -1, 1, 1, -1, -1};
+            vector<int> dely = {-1, 1, 0, 0, -1, 1, 1, -1};
+            for (int i = 0; i < 8; i++) {
+                int newx = x + delx[i];
+                int newy = y + dely[i];
+                if (newx >= 0 && newy >= 0 && newx < n && newy < n &&
+                    grid[newx][newy] == 0 && dist[newx][newy] > d + 1) {
+                    dist[newx][newy] = d + 1;
+                    q.push({newx, newy});
                 }
             }
         }
-
-        if(dist[n-1][m-1]!=1e9){
-            return dist[n-1][m-1];
-        }
-        return -1;
+        if (dist[n - 1][n - 1] == INT_MAX)
+            return -1;
+        else
+            return dist[n - 1][n - 1];
     }
 };
