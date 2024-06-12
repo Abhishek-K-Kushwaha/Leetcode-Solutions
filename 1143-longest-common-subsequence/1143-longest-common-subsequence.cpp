d2ece688cbf17fbd041643ae4bcc03e5e2c968ba
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-06-12 19:23:36 +0530

    Time: 18 ms (82.95%), Space: 25.7 MB (52.18%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
-    int f(int ind1, int ind2, string & text1, string& text2, vector<vector<int>>& dp){
-        if (ind1 < 0 || ind2 < 0) return 0;
-        if (dp[ind1][ind2] != -1) return dp[ind1][ind2];
-        if (text1[ind1] == text2[ind2]) 
-            return dp[ind1][ind2] = 1 + f(ind1-1, ind2-1, text1, text2, dp);
-        return dp[ind1][ind2] = 0 + 
-            max(f(ind1-1,ind2, text1, text2, dp), f(ind1, ind2-1, text1, text2, dp));
-    }
     int longestCommonSubsequence(string text1, string text2) {
         int len1 = text1.size();
         int len2 = text2.size();
-        vector<vector<int>> dp(len1,vector<int>(len2,-1));
-        return f(len1-1,len2-1, text1, text2, dp);
+        vector<vector<int>> dp(len1, vector<int>(len2, 0));
+        int flag = 0;
+        for (int i = 0; i < len2; i++) {
+            if (flag || text1[0] == text2[i]) {
+                dp[0][i] = 1;
+                flag = 1;
+            }
+        }
+        flag = 0;
+        for (int i = 0; i < len1; i++) {
+            if (flag || text2[0] == text1[i]) {
+                dp[i][0] = 1;
+                flag = 1;
+            }
+        }
+        for (int ind1 = 1; ind1 < len1; ind1++) {
+            for (int ind2 = 1; ind2 < len2; ind2++) {
+                if (text1[ind1] == text2[ind2])
+                    dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+                else {
+                    dp[ind1][ind2] =
+                        0 + max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+                }
+                // cout<< ind1<< " "<< ind2<< " val "<<dp[ind1][ind2]<<" ";
+            }
+        }
+        return dp[len1 - 1][len2 - 1];
     }
 };
