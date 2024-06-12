ba98e790ad8d323b885ecbc0cc5df16d2f26967a
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-06-12 12:15:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        int mod = 1e9 + 7;
+        vector<vector<pair<int, int>>> adj(n);
+        for (auto eg : roads) {
+            int u = eg[0];
+            int v = eg[1];
+            int w = eg[2];
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w});
+        }
+        vector<int> dist(n, INT_MAX);
+        vector<int> ways(n, 0);
+        dist[0] = 0;
+        ways[0] = 1;
+        set<pair<int, int>> st; //{distance,node}
+        st.insert({0, 0});
+        while (!st.empty()) {
+            auto it = *(st.begin());
+            st.erase(it);
+            int d = it.first;
+            int node = it.second;
+            int w = ways[node];
+            for (auto nxt : adj[node]) {
+                int nxtnode = nxt.first;
+                int nxtd = nxt.second;
+                if (nxtd + d == dist[nxtnode]) {
+                    ways[nxtnode] += w;
+                    ways[nxtnode] %= mod;
+                } else if (nxtd + d < dist[nxtnode]) {
+                    ways[nxtnode] = (w % mod);
+                    dist[nxtnode] = d + nxtd;
+                    st.insert({d + nxtd, nxtnode});
+                }
+            }
+        }
+        return ways[n - 1];
+    }
+};
