33ed43531ac289930c9f008cf4343c7c21d6e8e6
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-06-14 18:26:12 +0530

    Time: 23 ms (39.71%), Space: 31.3 MB (33.54%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    int f(int i, int j, string& s, string& t, vector<vector<int>>& dp) {
-        if (j < 0)
-            return 1;
-        if (i < 0)
-            return 0;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        int pick = 0;
-        if (s[i] == t[j])
-            pick = f(i - 1, j - 1, s, t, dp);
-        int notpick = f(i - 1, j, s, t, dp);
-        return dp[i][j] = pick + notpick;
-    }
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return f(n - 1, m - 1, s, t, dp);
+        vector<vector<double>> dp(n + 1, vector<double>(m + 1, 0));
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                double pick = 0;
+                if (s[i - 1] == t[j - 1])
+                    pick = dp[i - 1][j - 1];
+                double notpick = dp[i - 1][j];
+                dp[i][j] = pick + notpick;
+            }
+        }
+        return (int)dp[n][m];
     }
 };
