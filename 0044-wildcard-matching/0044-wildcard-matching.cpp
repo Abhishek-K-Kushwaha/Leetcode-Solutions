f761febf4195253965def497fc82e7fd69d426a6
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-06-14 23:17:20 +0530

    Time: 65 ms (36.12%), Space: 15.6 MB (65.36%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,33 +1,27 @@
 #include <cstring>
 class Solution {
 public:
-    bool f(int i, int j, string& s, string& p, vector<vector<int>>& dp) {
-        if (j < 0 && i < 0)
-            return true;
-        if (j < 0 && i >= 0)
-            return false;
-        if (j >= 0 && i < 0) {
-            for (int it = 0; it <= j; it++) {
-                if (p[it] != '*')
-                    return false;
-            }
-            return true;
-        }
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        if (p[j] == '*') {
-            bool notmatch = f(i, j - 1, s, p, dp);
-            bool match = f(i - 1, j, s, p, dp);
-            return dp[i][j] = (notmatch || match);
-        }
-        if (p[j] == '?' || s[i] == p[j])
-            return dp[i][j] = f(i - 1, j - 1, s, p, dp);
-        return dp[i][j] = false;
-    }
     bool isMatch(string s, string p) {
         int n = s.size();
-        int m = p.length();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return f(n - 1, m - 1, s, p, dp);
+        int m = p.size();
+        vector<vector<bool>> dp(n+1, vector<bool>(m+1, false));
+        dp[0][0] = true;
+        int j = 1;
+        while (p[j-1] == '*'){
+            dp[0][j] = true;
+            j++;
+        }
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= m; j++){
+                if (p[j-1] == '*') {
+                    bool notmatch = dp[i][j-1]; 
+                    bool match = dp[i-1][j]; 
+                    dp[i][j] = (notmatch || match);
+                }
+                else if (p[j-1] == '?' || s[i-1] == p[j-1])
+                    dp[i][j] = dp[i-1][j-1]; 
+            }
+        }
+        return dp[n][m];
     }
 };
