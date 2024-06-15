522e6034cf3a347f08634c45a351feb2cd15511b
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-06-15 18:08:24 +0530

    Time: 3 ms (72.15%), Space: 13.4 MB (87.5%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -4,21 +4,18 @@ public:
         int n = prices.size();
         vector<int> prev(3, 0), nxt(3);
         for (int ind = n - 1; ind >= 0; ind--) {
-            for (int state = 0; state <= 2; state++) {
-                if (state == 1) { // Can buy
-                    int buy = -prices[ind] + prev[0];
-                    int notbuy = prev[1];
-                    nxt[state] = max(buy, notbuy);
-                } 
-                else if (state == 0) { // Can sell
-                    int sell = prices[ind] + prev[2];
-                    int notsell = prev[0];
-                    nxt[state] = max(sell, notsell);
-                } 
-                else { // Can buy next day (state = 2)
-                    nxt[state] = prev[1];
-                }
-            }
+            //if (state == 1) Can buy
+            int buy = -prices[ind] + prev[0];
+            int notbuy = prev[1];
+            nxt[1] = max(buy, notbuy);
+
+            //else if (state == 0) Can sell
+            int sell = prices[ind] + prev[2];
+            int notsell = prev[0];
+            nxt[0] = max(sell, notsell);
+            
+            //else Can buy next day (state == 2)
+            nxt[2] = prev[1];
             prev = nxt;
         }
         return nxt[1];
