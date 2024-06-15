3cdd27800b0bf90548171aacddc3aa9a91ab41b9
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-06-15 19:04:12 +0530

    Time: 82 ms (65.34%), Space: 57.3 MB (81.73%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    int f(int ind, int canbuy, vector<int>& prices, int n, int fee, vector<vector<int>>& dp) {
-        if (ind == n)
-            return 0;
-        if (dp[ind][canbuy] != -1) return dp[ind][canbuy];
-        if (canbuy) {
-            int buy = -prices[ind] + f(ind + 1, 0, prices, n, fee, dp);
-            int notbuy = f(ind + 1, 1, prices, n, fee, dp);
-            return dp[ind][canbuy] = max(buy, notbuy);
-        } else {
-            int sell = prices[ind] - fee + f(ind + 1, 1, prices, n, fee, dp);
-            int notsell = f(ind + 1, 0, prices, n, fee, dp);
-            return dp[ind][canbuy] = max(sell, notsell);
-        }
-    }
-
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        return f(0, 1, prices, n, fee, dp);
+        vector<int> prev(2,0), nxt(2);
+        for (int ind = n-1; ind >= 0; ind--){
+            //if (canbuy == 1) {
+            int buy = -prices[ind] + prev[0]; 
+            int notbuy = prev[1]; 
+            nxt[1] = max(buy, notbuy);
+
+            // else if (canbuy == 0)
+            int sell = prices[ind] - fee + prev[1]; 
+            int notsell = prev[0]; 
+            nxt[0] = max(sell, notsell);
+            prev = nxt;
+        }
+        return prev[1];
     }
 };
