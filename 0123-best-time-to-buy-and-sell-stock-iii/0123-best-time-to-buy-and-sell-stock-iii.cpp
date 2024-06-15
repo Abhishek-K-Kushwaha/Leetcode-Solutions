0b97773125912cd5ac3bd6fdedeceae8d8125fea
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-06-15 16:44:15 +0530

    Time: 107 ms (77.51%), Space: 77.5 MB (99.32%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
-    int f(int ind, int cnt, vector<int>& prices, int n, vector<vector<int>>& dp) {
-        if (ind == n || cnt >= 4)
-            return 0;
-        if (dp[ind][cnt] != -1)
-            return dp[ind][cnt];
-        if (cnt % 2) { // cannot buy
-            int sell = prices[ind] + f(ind + 1, cnt + 1, prices, n, dp);
-            int notsell = f(ind + 1, cnt, prices, n, dp);
-            return dp[ind][cnt] = max(sell, notsell);
-        } else { // can buy
-            int buy = -prices[ind] + f(ind + 1, cnt + 1, prices, n, dp);
-            int notbuy = f(ind + 1, cnt, prices, n, dp);
-            return dp[ind][cnt] = max(buy, notbuy);
-        }
-    }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n+1, vector<int>(5, 0));
+        vector<int> prev(5,0);
         for (int ind = n-1; ind >= 0; ind--){
             for (int cnt = 3; cnt >= 0; cnt--){
                 if (cnt % 2) { // cannot buy
-                    int sell = prices[ind] + dp[ind+1][cnt+1]; //f(ind + 1, cnt + 1, prices, n, dp);
-                    int notsell = dp[ind+1][cnt]; //f(ind + 1, cnt, prices, n, dp);
-                    dp[ind][cnt] = max(sell, notsell);
+                    int sell = prices[ind] + prev[cnt+1]; 
+                    int notsell = prev[cnt]; 
+                    prev[cnt] = max(sell, notsell);
                 } else { // can buy
-                    int buy = -prices[ind] + dp[ind+1][cnt+1]; //f(ind + 1, cnt + 1, prices, n, dp);
-                    int notbuy = dp[ind+1][cnt]; //f(ind + 1, cnt, prices, n, dp);
-                    dp[ind][cnt] = max(buy, notbuy);
+                    int buy = -prices[ind] + prev[cnt+1]; 
+                    int notbuy = prev[cnt]; 
+                    prev[cnt] = max(buy, notbuy);
                 }
             }
         }
-        return dp[0][0];
+        return prev[0];
     }
 };
