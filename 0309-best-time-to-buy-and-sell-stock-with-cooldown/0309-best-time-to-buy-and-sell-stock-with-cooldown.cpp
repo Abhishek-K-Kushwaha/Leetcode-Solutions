4f0dd03dc767e66679fca51c80ee42b849ef0424
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-06-15 17:53:26 +0530

    Time: 4 ms (54.21%), Space: 13.9 MB (69.39%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    int f(int ind, int state, vector<int>& prices, int n, vector<vector<int>>& dp) {
-        if (ind == n)
-            return 0;
-        if (dp[ind][state] != -1)
-            return dp[ind][state];
-        if (state == 1) { // Can buy
-            int buy = -prices[ind] + f(ind + 1, 0, prices, n, dp);
-            int notbuy = f(ind + 1, 1, prices, n, dp);
-            return dp[ind][state] = max(buy, notbuy);
-        } else if (state == 0) { // Can sell
-            int sell = prices[ind] + f(ind + 1, 2, prices, n, dp);
-            int notsell = f(ind + 1, 0, prices, n, dp);
-            return dp[ind][state] = max(sell, notsell);
-        } else { // Can buy next day (state = 2)
-            return dp[ind][state] = f(ind + 1, 1, prices, n, dp);
-        }
-    }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(3, -1));
-        return f(0, 1, prices, n, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(3, 0));
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int state = 0; state <= 2; state++) {
+                if (state == 1) { // Can buy
+                    int buy = -prices[ind] + dp[ind + 1][0];
+                    int notbuy = dp[ind + 1][1];
+                    dp[ind][state] = max(buy, notbuy);
+                } else if (state == 0) { // Can sell
+                    int sell = prices[ind] + dp[ind + 1][2];
+                    int notsell = dp[ind + 1][0];
+                    dp[ind][state] = max(sell, notsell);
+                } else { // Can buy next day (state = 2)
+                    dp[ind][state] = dp[ind + 1][1];
+                }
+            }
+        }
+        return dp[0][1];
     }
 };
