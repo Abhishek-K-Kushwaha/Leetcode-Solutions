3a650bbd389b2d31e84f0fc55334dbe10246ab8f
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-06-21 19:54:11 +0530

    Time: 1510 ms (38.24%), Space: 7.7 MB (73.09%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -8,21 +8,19 @@ public:
         }
         return true;
     }
-    int f(int i, string & s, int n, vector<int>& dp){
-        if (i == n) return 0;
-        if (dp[i] != -1) return dp[i];
-        int ans = n;
-        for (int j = i; j < n; j++){
-            if (isPalindrome(s,i,j)){
-                int cuts = 1 + f(j+1, s, n, dp);
-                ans = min(ans, cuts);
-            }
-        }
-        return dp[i] = ans;
-    }
     int minCut(string s) {
         int n = s.size();
-        vector<int> dp(n, -1);
-        return f(0, s, n, dp)-1;
+        vector<int> dp(n+1, 0);
+        for (int i = n-1; i >= 0; i--){
+            int ans = n;
+            for (int j = i; j < n; j++){
+                if (isPalindrome(s,i,j)){
+                    int cuts = 1 + dp[j+1]; // f(j+1, s, n, dp);
+                    ans = min(ans, cuts);
+                }
+            }
+            dp[i] = ans;
+        }
+        return dp[0] - 1;
     }
 };
