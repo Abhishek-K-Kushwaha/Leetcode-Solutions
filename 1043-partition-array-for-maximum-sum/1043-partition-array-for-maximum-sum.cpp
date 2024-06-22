d63d2096dce6872f7fe18f814f24bdcab53f5285
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-06-22 15:53:01 +0530

    Time: 53 ms (8.19%), Space: 37.5 MB (5.01%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,37 +1,23 @@
 class Solution {
 public:
-    // int maxele(int i, int j, vector<int>& arr){
-    //     if (i == j) return arr[i];
-    //     if (i > j) return 0;
-    //     return max({arr[i], arr[j], maxele(i+1, j-1, arr)});
-    // }
+    int maxele(int i, int j, vector<int>& arr, vector<vector<int>>& maxdp){
+        if (i > j) return 0;
+        if (i == j) return arr[i];
+        if (maxdp[i][j] != -1) return maxdp[i][j];
+        return maxdp[i][j] = max({arr[i], arr[j], maxele(i+1, j-1, arr, maxdp)});
+    }
 
-    // int f(int i, int k, vector<int>& arr, int n, vector<int>& dp){
-    //     if (i == n) return 0;
-    //     //if (i == n-1) return dp[i] = arr[n-1];
-    //     if (dp[i] != -1) return dp[i];
-    //     int ans = 0;
-    //     for (int j = 1; j <= k; j++){
-    //         if (i + j <= n){
-    //             //auto it = max_element(arr.begin() + i, arr.begin() + i + j);
-    //             //int itval = *it;
-    //             int itval = maxele(i, i+j-1, arr);
-    //             int val = j*itval + f(i+j, k, arr, n, dp);
-    //             ans = max(ans, val);
-    //         }
-    //     }
-    //     return dp[i] = ans;
-    // }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
+        vector<vector<int>> maxdp(n, vector<int> (n, -1));
         vector<int> dp(n + 1, 0);
         for (int i = n-1; i >= 0; i-- ){
             int ans = 0;
             for (int j = 1; j <= k; j++){
                 if (i + j <= n){
-                    auto it = max_element(arr.begin() + i, arr.begin() + i + j);
-                    int itval = *it;
-                    //int itval = maxele(i, i+j-1, arr);
+                    //auto it = max_element(arr.begin() + i, arr.begin() + i + j);
+                    //int itval = *it;
+                    int itval = maxele(i, i+j-1, arr, maxdp);
                     int val = j*itval + dp[i+j]; //f(i+j, k, arr, n, dp);
                     ans = max(ans, val);
                 }
