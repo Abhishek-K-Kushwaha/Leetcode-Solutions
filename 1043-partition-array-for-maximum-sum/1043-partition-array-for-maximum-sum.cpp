6e46aae03dac474aa0d5cba6b1a2235cd3cda708
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-06-22 15:53:47 +0530

    Time: 18 ms (11.5%), Space: 36.4 MB (5.01%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    int maxele(int i, int j, vector<int>& arr, vector<vector<int>>& maxdp){
-        if (i > j) return 0;
-        if (i == j) return arr[i];
-        if (maxdp[i][j] != -1) return maxdp[i][j];
-        return maxdp[i][j] = max({arr[i], arr[j], maxele(i+1, j-1, arr, maxdp)});
-    }
+    // int maxele(int i, int j, vector<int>& arr, vector<vector<int>>& maxdp){
+    //     if (i > j) return 0;
+    //     if (i == j) return arr[i];
+    //     if (maxdp[i][j] != -1) return maxdp[i][j];
+    //     return maxdp[i][j] = max({arr[i], arr[j], maxele(i+1, j-1, arr, maxdp)});
+    // }
 
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
@@ -13,12 +13,14 @@ public:
         vector<int> dp(n + 1, 0);
         for (int i = n-1; i >= 0; i-- ){
             int ans = 0;
+            int maxval = 0;
             for (int j = 1; j <= k; j++){
                 if (i + j <= n){
                     //auto it = max_element(arr.begin() + i, arr.begin() + i + j);
                     //int itval = *it;
-                    int itval = maxele(i, i+j-1, arr, maxdp);
-                    int val = j*itval + dp[i+j]; //f(i+j, k, arr, n, dp);
+                    maxval = max(maxval, arr[i+j-1]);
+                    //int itval = maxele(i, i+j-1, arr, maxdp);
+                    int val = j*maxval + dp[i+j]; //f(i+j, k, arr, n, dp);
                     ans = max(ans, val);
                 }
             }
