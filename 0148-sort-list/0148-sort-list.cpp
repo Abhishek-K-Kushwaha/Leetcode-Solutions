cacb3bc75d22d7a896b15158cd687311c8e11898
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-06-25 17:06:17 +0530

    Time: 159 ms (11.64%), Space: 73.5 MB (21.15%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,11 +10,12 @@
  */
 class Solution {
 public:
-    ListNode* findMiddle(ListNode* head){
-        if (head == nullptr || head->next == nullptr) return head;
+    ListNode* findMiddle(ListNode* head) {
+        if (head == nullptr || head->next == nullptr)
+            return head;
         ListNode* slow = head;
         ListNode* fast = head->next->next;
-        while (fast && fast->next){
+        while (fast && fast->next) {
             fast = fast->next->next;
             slow = slow->next;
         }
@@ -23,28 +24,27 @@ public:
     ListNode* merge(ListNode* temp1, ListNode* temp2) {
         ListNode* temp = new ListNode(-1);
         ListNode* headofhead = temp;
-        while (temp1 && temp2){
-            if (temp1->val < temp2->val){
+        while (temp1 && temp2) {
+            if (temp1->val < temp2->val) {
                 temp->next = temp1;
                 temp1 = temp1->next;
-            }
-            else {
+            } else {
                 temp->next = temp2;
                 temp2 = temp2->next;
             }
             temp = temp->next;
         }
-        if (temp1 == nullptr){
+        if (temp1 == nullptr) {
             temp->next = temp2;
-        }
-        else{
+        } else {
             temp->next = temp1;
         }
         return headofhead->next;
     }
 
     ListNode* sortList(ListNode* head) {
-        if (head == nullptr || head->next == nullptr) return head;
+        if (head == nullptr || head->next == nullptr)
+            return head;
         ListNode* mid = findMiddle(head);
         ListNode* lefthead = head;
         ListNode* righthead = mid->next;
