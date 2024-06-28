828e0ede003dee238c6f2345f49af422602996b3
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-06-28 18:08:28 +0530

    Time: 2 ms (45%), Space: 11.9 MB (11.76%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -6,19 +6,24 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    bool f(TreeNode* p, TreeNode* q){
-        if (p == nullptr && q == nullptr) return true;
-        if (p == nullptr) return false;
-        if (q == nullptr) return false;
-        if (p->val != q->val) return false;
-        return (f(p->left, q->left) && f(p->right, q->right));
+    bool f(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr)
+            return true;
+        if (p == nullptr)
+            return false;
+        if (q == nullptr)
+            return false;
+        if (p->val != q->val)
+            return false;
+        return f(p->left, q->left) && f(p->right, q->right);
     }
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        return f(p, q);
+    bool isSameTree(TreeNode* p, TreeNode* q) { 
+        return f(p, q); 
     }
 };
