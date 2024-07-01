920168732fbfd2e38f315bf440018e2ab223a25b
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-01 18:49:40 +0530

    Time: 3 ms (83.11%), Space: 14.9 MB (63.66%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -12,7 +12,6 @@ public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
         unordered_map<TreeNode*, TreeNode*> parent;
         queue<TreeNode*> q;
-        //parent[root] = nullptr;
         q.push(root);
         while (!q.empty()) {
             TreeNode* node = q.front();
