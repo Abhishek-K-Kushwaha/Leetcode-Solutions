f6573968a7342369423dffeabb55e29c1caea203
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-04 16:57:34 +0530

    Time: 7 ms (75.73%), Space: 20.1 MB (61.31%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -6,21 +6,22 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    bool f(TreeNode* node, long long min, long long max){
-        if (node == nullptr) return true;
-        if (node->val > max || node->val < min) return false;
-        //bool ans = true;
-        bool ans = f(node->left, min, (long long) node->val-1);
-        if (!ans) return false;
-        ans = ans & f(node->right,(long long) node->val+1, max);
+    bool f(TreeNode* node, long long min, long long max) {
+        if (node == nullptr)
+            return true;
+        if (node->val > max || node->val < min)
+            return false;
+        bool ans = f(node->left, min, (long long)node->val - 1);
+        if (!ans)
+            return false;
+        ans = ans & f(node->right, (long long)node->val + 1, max);
         return ans;
     }
-    bool isValidBST(TreeNode* root) {
-        return f(root, INT_MIN, INT_MAX);
-    }
+    bool isValidBST(TreeNode* root) { return f(root, INT_MIN, INT_MAX); }
 };
