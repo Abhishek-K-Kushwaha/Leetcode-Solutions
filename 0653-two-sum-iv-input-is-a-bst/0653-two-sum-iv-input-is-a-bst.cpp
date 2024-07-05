0718c460e532028abf97eefb5070984fe92187c4
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-05 16:19:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -9,25 +9,48 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class BSTIterator {
+public:
+    stack<TreeNode*> stk;
+    bool reverse = true; //if true-> before element else next element
+    void pushAll(TreeNode* node){
+        while (node){
+            stk.push(node);
+            if (reverse) node = node->right;
+            else node = node->left;
+        }
+    }
+    BSTIterator(TreeNode* root, bool isreverse) {
+        reverse = isreverse;
+        pushAll(root);
+    }
+    
+    int next() {
+        TreeNode* node = stk.top();
+        stk.pop();
+        if (reverse) pushAll(node->right);
+        else pushAll(node->left);
+        return node->val;
+    }
+    
+    bool hasNext() {
+        return !stk.empty();
+    }
+};
+
 class Solution {
 public:
-    bool findSecond(TreeNode* root, int target, TreeNode* first){
-        while(root){
-            if (root->val == target && root != first) return true;
-            else if (root->val > target) root = root->left;
-            else root = root->right;
-        }
-        return false;
-    }
     bool findTarget(TreeNode* root, int k) {
-        stack<TreeNode*> stk;
-        stk.push(root);
-        while(!stk.empty()){
-            TreeNode* first = stk.top();
-            stk.pop();
-            if (findSecond(root, k - first->val, first)) return true;
-            if (first->left) stk.push(first->left);
-            if (first->right) stk.push(first->right);
+        BSTIterator before(root, true);
+        BSTIterator after(root, false);
+        int i = after.next(); // smallest element
+        int j = before.next(); // largest element
+        //cout<< i << " " << j << " | ";
+        while (i < j){
+            //cout<< i << " " << j << " | ";
+            if (i + j == k) return true;
+            else if (i + j > k) i = before.next();
+            else j = after.next();
         }
         return false;
     }
