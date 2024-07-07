dd1dfa3664891ad545ef67826c114febbed4425e
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-07 13:02:31 +0530

    Time: 2 ms (53.84%), Space: 7.1 MB (72.3%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.cpp
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -3,11 +3,12 @@ public:
     int numWaterBottles(int numBottles, int numExchange) {
         int ans = numBottles;
         int empty = numBottles;
-        while (true){
-            int newBottles = empty/numExchange;
-            if (newBottles == 0) break;
+        while (true) {
+            int newBottles = empty / numExchange;
+            if (newBottles == 0)
+                break;
             ans += newBottles;
-            empty = (empty - newBottles*numExchange) +newBottles;
+            empty = (empty - newBottles * numExchange) + newBottles;
         }
         return ans;
     }
