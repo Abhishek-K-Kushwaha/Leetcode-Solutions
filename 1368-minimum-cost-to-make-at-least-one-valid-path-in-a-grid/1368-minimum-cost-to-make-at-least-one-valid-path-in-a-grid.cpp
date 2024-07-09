9ee772940f66814d65c02d6193d949107a5fc943
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-09 19:03:56 +0530

    Time: 18 ms (96.5%), Space: 24 MB (40.94%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -3,33 +3,36 @@ public:
     int minCost(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq; // Min-heap {cost, {i, j}}
-        vector<vector<int>> costmat(m, vector<int>(n, INT_MAX));
-        costmat[0][0] = 0;
-        pq.push({0, {0, 0}});
+        vector<vector<int>> cost(m, vector<int>(n, INT_MAX));
+        deque<pair<int, int>> dq;
+        cost[0][0] = 0;
+        dq.push_front({0, 0});
+        
         vector<int> dx = {0, 0, 1, -1};
         vector<int> dy = {1, -1, 0, 0};
 
-        while (!pq.empty()) {
-            auto it = pq.top();
-            pq.pop();
-            int cost = it.first;
-            int i = it.second.first;
-            int j = it.second.second;
-            if (i == m - 1 && j == n - 1) return cost;
-
-            for (int k = 0; k < 4; k++) {
+        while (!dq.empty()) {
+            auto [i, j] = dq.front();
+            dq.pop_front();
+            
+            for (int k = 0; k < 4; ++k) {
                 int newi = i + dx[k];
                 int newj = j + dy[k];
+                
                 if (newi >= 0 && newi < m && newj >= 0 && newj < n) {
-                    int newCost = (k + 1 == grid[i][j]) ? cost : cost + 1;
-                    if (newCost < costmat[newi][newj]) {
-                        costmat[newi][newj] = newCost;
-                        pq.push({newCost, {newi, newj}});
+                    int newCost = cost[i][j] + (grid[i][j] != k + 1);
+                    
+                    if (newCost < cost[newi][newj]) {
+                        cost[newi][newj] = newCost;
+                        if (grid[i][j] == k + 1) {
+                            dq.push_front({newi, newj});
+                        } else {
+                            dq.push_back({newi, newj});
+                        }
                     }
                 }
             }
         }
-        return -1;
+        return cost[m - 1][n - 1];
     }
 };
