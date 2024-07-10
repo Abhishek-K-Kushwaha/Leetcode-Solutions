de860cfceb4e46743bac4cc0180acc8fac84b1ed
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-10 12:42:36 +0530

    Time: 6 ms (40.12%), Space: 14 MB (61.11%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.cpp
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     int minOperations(vector<string>& logs) {
         int lvl = 0;
-        for (auto log: logs){
-            if (log == "../"){
-                lvl = max(0, lvl-1);
-            }
-            else if (log == "./"){
+        for (auto log : logs) {
+            if (log == "../") {
+                lvl = max(0, lvl - 1);
+            } else if (log == "./") {
                 continue;
-            }
-            else{
+            } else {
                 lvl++;
             }
         }
