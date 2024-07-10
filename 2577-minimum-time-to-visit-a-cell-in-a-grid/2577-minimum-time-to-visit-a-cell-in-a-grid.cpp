d674ad3e38f6bd1364a3f5624f6c6226fb9433b7
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-10 15:06:33 +0530

    Time: 265 ms (94.46%), Space: 54.7 MB (72.32%) - LeetHub

--- 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.cpp
+++ 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.cpp
@@ -12,27 +12,28 @@ public:
             pq; //{time{x,y}}
         vector<vector<int>> time(m, vector<int>(n, INT_MAX));
         time[0][0] = 0;
-        if (grid[0][1] <= 1) {
-            pq.push({1, {0, 1}});
-            time[0][1] = 1;
-            if (grid[1][0] % 2) {
-                pq.push({grid[1][0], {1, 0}});
-                time[1][0] = grid[1][0];
-            } else {
-                pq.push({grid[1][0] + 1, {1, 0}});
-                time[1][0] = grid[1][0] + 1;
-            }
-        } else if (grid[1][0] <= 1) {
-            pq.push({1, {1, 0}});
-            time[1][0] = 1;
-            if (grid[0][1] % 2) {
-                pq.push({grid[0][1], {0, 1}});
-                time[0][1] = grid[0][1];
-            } else {
-                pq.push({grid[0][1] + 1, {0, 1}});
-                time[0][1] = grid[0][1] + 1;
-            }
-        }
+        pq.push({0,{0,0}});
+        // if (grid[0][1] <= 1) {
+        //     pq.push({1, {0, 1}});
+        //     time[0][1] = 1;
+        //     if (grid[1][0] % 2) {
+        //         pq.push({grid[1][0], {1, 0}});
+        //         time[1][0] = grid[1][0];
+        //     } else {
+        //         pq.push({grid[1][0] + 1, {1, 0}});
+        //         time[1][0] = grid[1][0] + 1;
+        //     }
+        // } else if (grid[1][0] <= 1) {
+        //     pq.push({1, {1, 0}});
+        //     time[1][0] = 1;
+        //     if (grid[0][1] % 2) {
+        //         pq.push({grid[0][1], {0, 1}});
+        //         time[0][1] = grid[0][1];
+        //     } else {
+        //         pq.push({grid[0][1] + 1, {0, 1}});
+        //         time[0][1] = grid[0][1] + 1;
+        //     }
+        // }
         vector<int> delx = {0, 0, -1, 1};
         vector<int> dely = {-1, 1, 0, 0};
         while (!pq.empty()) {
