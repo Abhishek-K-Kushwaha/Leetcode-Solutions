5dc9487b3d242282ce41fc5483b536bfe1a9472a
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-10 10:25:48 +0530

    Time: 15 ms (75.45%), Space: 33.4 MB (41.74%) - LeetHub

---
+++ 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void f(string s, int i, int n, vector<string>& ans) {
+        if (i == n) {
+            ans.push_back(s);
+            return;
+        }
+
+        f(s + "1", i + 1, n, ans);
+        if (i == 0 || s[i - 1] == '1') {
+            f(s + "0", i + 1, n, ans);
+        }
+    }
+    vector<string> validStrings(int n) {
+        vector<string> ans;
+        f("", 0, n, ans);
+        return ans;
+    }
+};
