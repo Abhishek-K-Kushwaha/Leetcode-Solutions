3a4b39e4ddbb4b968c7e3e7f66ef0c9074ba8580
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-12 20:15:17 +0530

    Time: 60 ms (67.27%), Space: 24.9 MB (11.17%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -8,18 +8,14 @@ public:
         for (int i = 0; i < m; i++){
             for (int j = 0; j < n; j++){
                 dp[i][j] = matrix[i][j] - '0';
-                cout<< dp[i][j] << " ";
                 ans = max(ans, dp[i][j]);
             }
-            cout<< "\n";
         }
         for (int r = 1; r < m; r++){
             for (int c = 1; c < n; c++){
                 if (dp[r][c]) dp[r][c] = dp[r][c] + min({dp[r-1][c-1], dp[r][c-1], dp[r-1][c]});
-                cout<< dp[r][c] << " ";
                 ans = max(ans, dp[r][c]);
             }
-            cout<< "\n";
         }
         return ans*ans;
     }
