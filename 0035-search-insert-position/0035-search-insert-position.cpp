3d441a1d643e1988f3200fcd38e2d28724c9c1fc
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-12 14:43:27 +0530

    Time: 8 ms (5.99%), Space: 12.4 MB (8.05%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,8 +4,8 @@ public:
         int n = nums.size();
         int i = 0; 
         int j = n-1;
-        while (i < j){
-            int mid = (i + j)/2;
+        while (i <= j){
+            int mid = i + (j - i)/2;
             if (nums[mid] == target) return mid;
             if (nums[mid] < target){
                 i = mid + 1;
@@ -14,7 +14,7 @@ public:
                 j = mid - 1;
             }
         }
-        if (i < n && nums[i] < target) return i + 1;
-        else return i;
+
+        return i;
     }
 };
