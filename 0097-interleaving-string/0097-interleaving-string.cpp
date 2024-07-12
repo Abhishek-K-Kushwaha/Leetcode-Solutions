709aa4e12aa2e214172cd7f81a00fd28a987c2c3
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-12 21:39:18 +0530

    Time: 0 ms (100%), Space: 8.7 MB (45.93%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool f(int i, int j, string& s1, string& s2, string& s3, vector<vector<int>>& dp){
         int k = i + j;
-        if (k == s3.size() && (i == s1.size() || j == s2.size())) return true;
+        if (k == s3.size()) return true;
         if (i < s1.size() && j < s2.size() && dp[i][j] != -1) return dp[i][j];
         bool first = false;
         bool second = false;
@@ -20,6 +20,8 @@ public:
         int l = s3.size();
         if (m+n != l) return false;
         vector<vector<int>> dp(m+1, vector<int> (n+1, -1));
+        //for (int i = )
+
         return f(0,0,s1,s2,s3, dp);
     }
 };
