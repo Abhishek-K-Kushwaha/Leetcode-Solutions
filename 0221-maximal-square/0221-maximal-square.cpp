fa0d17701cd77314729bf8fe7d594e8ac7f4ea01
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-12 20:23:50 +0530

    Time: 62 ms (57.76%), Space: 24.8 MB (11.17%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -5,12 +5,6 @@ public:
         int n = matrix[0].size();
         vector<vector<int>> dp(m, vector<int> (n,0));
         int ans = 0;
-        // for (int i = 0; i < m; i++){
-        //     for (int j = 0; j < n; j++){
-        //         dp[i][j] = matrix[i][j] - '0';
-        //         ans = max(ans, dp[i][j]);
-        //     }
-        // }
         for (int r = 0; r < m; r++){
             for (int c = 0; c < n; c++){
                 if (matrix[r][c] == '1'){ 
