012e8390a81d9a9760d80db51920347f0f17fe5a
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-12 12:00:05 +0530

    Time: 52 ms (96.34%), Space: 18.1 MB (81.3%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
     int maximumGain(string s, int x, int y) {
-        stack<char> stk;
-        char first, second;
-        if (x > y){
-            first = 'a';
-            second = 'b';
-        }
-        else{
-            first = 'b';
-            second = 'a';
-            swap(x,y);
-        }
-        int ans = 0;
-        for (auto c:s){
-            if (!stk.empty() && stk.top() == first && c == second){
-                ans += x;
-                stk.pop();
-            }
-            else{
-                stk.push(c);
-            }
-        }
-        stack<char> st;
-        while(!stk.empty()){
-            char c = stk.top();
-            if (!st.empty() && st.top() == first && c == second){
-                ans += y;
-                st.pop();
-            }
-            else st.push(c);
-            stk.pop();
-        }
-        return ans;
+        int aCount = 0;
+        int bCount = 0;
+        int lesser = min(x, y);
+        int result = 0;
 
+        for (char c : s) {
+            if (c > 'b') {
+                result += min(aCount, bCount) * lesser;
+                aCount = 0;
+                bCount = 0;
+            } else if (c == 'a') {
+                if (x < y && bCount > 0) {
+                    bCount--;
+                    result += y;
+                } else {
+                    aCount++;
+                }
+            } else {
+                if (x > y && aCount > 0) {
+                    aCount--;
+                    result += x;
+                } else {
+                    bCount++;
+                }
+            }
+        }
+
+        result += min(aCount, bCount) * lesser;
+        return result;
     }
 };
