cff917486187de40b74e480b259b068e69bd9715
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-13 14:41:23 +0530

    Time: 559 ms (9.54%), Space: 244.4 MB (26.33%) - LeetHub

--- 2751-robot-collisions/2751-robot-collisions.cpp
+++ 2751-robot-collisions/2751-robot-collisions.cpp
@@ -9,6 +9,7 @@ public:
         stack<int> stk; // index
         for (auto it:dict){
             //int pos = it.first;
+            cout<< it.second << " ";
             int ind = it.second;
             if (stk.empty() || directions[stk.top()] == 'L' || directions[ind] == 'R'){
                 // if (directions[ind] == 'L') stk.push({health[ind], -1});
@@ -23,13 +24,14 @@ public:
                     healths[stk.top()]--;
                 }
                 else{
-                    while(!stk.empty() && healths[stk.top()] < healths[ind]){
+                    while(!stk.empty() && healths[stk.top()] < healths[ind] && directions[stk.top()] == 'R'){
                         healths[ind]--;
                         stk.pop();
                     }
                     if (stk.empty()) stk.push(ind);
-                    else if (healths[stk.top()] == healths[ind]) stk.pop();
-                    else healths[stk.top()]--;
+                    else if (healths[stk.top()] == healths[ind] && directions[stk.top()] == 'R') stk.pop();
+                    else if (directions[stk.top()] == 'R') healths[stk.top()]--;
+                    else stk.push(ind);
                 }
             }
         }
