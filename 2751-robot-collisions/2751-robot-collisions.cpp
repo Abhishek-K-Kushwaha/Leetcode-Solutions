e51f54bc46c721c870d3f53177a254dc25145d85
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-13 14:40:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2751-robot-collisions/2751-robot-collisions.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
+        map<int,int> dict; //position --> index of robot
+        int n = positions.size();
+        for (int i = 0; i < n; i++){
+            dict[positions[i]] = i;
+        }
+        stack<int> stk; // index
+        for (auto it:dict){
+            //int pos = it.first;
+            int ind = it.second;
+            if (stk.empty() || directions[stk.top()] == 'L' || directions[ind] == 'R'){
+                // if (directions[ind] == 'L') stk.push({health[ind], -1});
+                // else stk.push({health[ind], 1});
+                stk.push(ind);
+            }
+            else{
+                if (healths[ind] == healths[stk.top()]){
+                    stk.pop();
+                }
+                else if (healths[ind] < healths[stk.top()]){
+                    healths[stk.top()]--;
+                }
+                else{
+                    while(!stk.empty() && healths[stk.top()] < healths[ind]){
+                        healths[ind]--;
+                        stk.pop();
+                    }
+                    if (stk.empty()) stk.push(ind);
+                    else if (healths[stk.top()] == healths[ind]) stk.pop();
+                    else healths[stk.top()]--;
+                }
+            }
+        }
+        //int len = stk.size();
+        set<int> ans;
+        //int i = len-1;
+        while(!stk.empty()){
+            ans.insert(stk.top());
+            stk.pop();
+            //i--;
+        }
+        vector<int> result;
+        for (int i:ans){
+            result.push_back(healths[i]);
+        }
+        return result;
+    }
+};
