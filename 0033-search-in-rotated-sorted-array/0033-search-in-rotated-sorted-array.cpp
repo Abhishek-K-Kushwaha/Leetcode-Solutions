85fc7a26113b59c1e41a1ba23a3b63d8884f5b8f
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-14 18:32:59 +0530

    Time: 3 ms (59.25%), Space: 14 MB (7.98%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -2,69 +2,28 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int n = nums.size();
-        if (n == 1){
-            if (nums[0] == target) return 0;
-            else return -1;
-        }
-        if (n == 2){
-            if (nums[0] == target) return 0;
-            if (nums[1] == target) return 1;
-            else return -1;
-        }
-        int pivot = -1;
-        if (nums[0] > nums[n-1]){
-            int i = 0;
-            int j = n-1;
-            while (i <= j){
-                int mid = i + (j-i)/2;
-                if (nums[mid] == target) return mid;
-                if (nums[mid] > nums[mid+1]){ 
-                    pivot = mid;
-                    break;
-                }
-                if (nums[mid] > nums[n-1]) i = mid+1;
-                else j = mid-1;
+        int left = 0, right = n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+
+            // Check if the left half is sorted
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+            // Otherwise, the right half must be sorted
+            else {
+                if (nums[mid] < target && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
             }
         }
-        if (pivot == -1){
-            int i = 0;
-            int j = n-1;
-            while (i <= j){
-                int mid = i + (j-i)/2;
-                if (nums[mid] == target){ 
-                    return mid;
-                }
-                if (nums[mid] > target) j = mid-1;
-                else i = mid+1;
-            }
-            return -1;
-        }
-        if (target < nums[0]){
-            int i = pivot+1;
-            int j = n-1;
-            while (i <= j){
-                int mid = i + (j-i)/2;
-                if (nums[mid] == target){ 
-                    return mid;
-                }
-                if (nums[mid] > target) j = mid-1;
-                else i = mid+1;
-            }
-            return -1;
-        }
-        else{
-            int i = 0;
-            int j = pivot;
-            while (i <= j){
-                int mid = i + (j-i)/2;
-                if (nums[mid] == target){ 
-                    return mid;
-                }
-                if (nums[mid] > target) j = mid-1;
-                else i = mid+1;
-            }
-            return -1;
-        }
-        return -1;
+
+        return -1; // Target not found
     }
 };
