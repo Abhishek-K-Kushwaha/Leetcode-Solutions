7bf64b4ef83d1619fed47e9d58ab50f5b38c69b4
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-14 18:25:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        if (n == 1){
+            if (nums[0] == target) return 0;
+            else return -1;
+        }
+        if (n == 2){
+            if (nums[0] == target) return 0;
+            if (nums[1] == target) return 1;
+            else return -1;
+        }
+        int pivot = -1;
+        if (nums[0] > nums[n-1]){
+            int i = 0;
+            int j = n-1;
+            while (i <= j){
+                int mid = i + (j-i)/2;
+                if (nums[mid] == target) return mid;
+                if (mid == 0 || (nums[mid-1] < nums[mid] && nums[mid] > nums[mid+1])){ 
+                    pivot = mid;
+                    break;
+                }
+                if (nums[mid] > nums[n-1]) i = mid+1;
+                if (nums[mid] < nums[n-1]) j = mid-1;
+            }
+        }
+        cout<< pivot;
+        if (pivot == -1){
+            int i = 0;
+            int j = n-1;
+            while (i <= j){
+                int mid = i + (j-i)/2;
+                if (nums[mid] == target){ 
+                    return mid;
+                }
+                if (nums[mid] > target) j = mid-1;
+                else i = mid+1;
+            }
+            return -1;
+        }
+        if (target < nums[0]){
+            int i = pivot+1;
+            int j = n-1;
+            while (i <= j){
+                int mid = i + (j-i)/2;
+                if (nums[mid] == target){ 
+                    return mid;
+                }
+                if (nums[mid] > target) j = mid-1;
+                else i = mid+1;
+            }
+            return -1;
+        }
+        else{
+            int i = 0;
+            int j = pivot;
+            while (i <= j){
+                int mid = i + (j-i)/2;
+                if (nums[mid] == target){ 
+                    return mid;
+                }
+                if (nums[mid] > target) j = mid-1;
+                else i = mid+1;
+            }
+            return -1;
+        }
+        return -1;
+    }
+};
