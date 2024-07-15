aa47aaea4918d5bad6367d6cc0e3a8f8346a0b3d
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-15 21:12:22 +0530

    Time: 152 ms (6.78%), Space: 70.7 MB (12.01%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -5,13 +5,6 @@ public:
         for(auto i: nums){
             freq[-i]++;
         }
-        // set<pair<int,int>> st;
-        // for (auto& it:freq){
-        //     st.insert(it);
-        //     if (st.size() > k){
-        //         st.erase(prev(st.end()));
-        //     }
-        // }
         for (auto& it: freq){
             if (k <= it.second) return -it.first;
             k -= it.second;
