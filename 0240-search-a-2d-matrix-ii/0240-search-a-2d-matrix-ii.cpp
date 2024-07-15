1563345b0b81f17c73f6f428d362b71f3f73280f
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-15 13:45:17 +0530

    Time: 44 ms (66.3%), Space: 17.7 MB (5.73%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -4,7 +4,7 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
         int up = 0, down = m-1, left = 0, right = n-1;
-        if (target < matrix[up][left] || target > matrix[down][right]) return false;
+        //if (target < matrix[up][left] || target > matrix[down][right]) return false;
         while (up <= down && left <= right){
             if (target > matrix[up][right]) up++;
             else if (target < matrix[up][right]) right--;
