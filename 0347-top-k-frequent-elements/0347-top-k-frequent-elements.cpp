0a0f1d2d85dc5eb5d6bd0cb0fd6ef7d3a2fe2284
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-15 19:29:39 +0530

    Time: 8 ms (88.37%), Space: 17.7 MB (16.97%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> freq;
+        set<pair<int,int>> st;
+        for (int i:nums){
+            freq[i]--;
+        }
+        for (auto it:freq){
+            st.insert({it.second, it.first});
+        }
+        vector<int> ans;
+        int i = 0;
+        for (auto it:st){
+            ans.push_back(it.second);
+            i++;
+            if (i == k) return ans;
+        }
+        return ans;
+    }
+};
