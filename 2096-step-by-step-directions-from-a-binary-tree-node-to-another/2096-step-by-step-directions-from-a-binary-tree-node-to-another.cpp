88f86d17930bc2e523efe7506493e11e650af1bf
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-16 12:58:35 +0530

    Time: 155 ms (57.65%), Space: 119.4 MB (40.98%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -6,24 +6,28 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    bool preorder(TreeNode* node, string& ans, int tar){
-        if (node == nullptr) return false;
-        if (node->val == tar) return true;
+    bool preorder(TreeNode* node, string& ans, int tar) {
+        if (node == nullptr)
+            return false;
+        if (node->val == tar)
+            return true;
 
         ans.push_back('L');
-        if (preorder(node->left, ans, tar)) return true;
+        if (preorder(node->left, ans, tar))
+            return true;
         ans.pop_back();
 
         ans.push_back('R');
-        if (preorder(node->right, ans, tar)) return true;
+        if (preorder(node->right, ans, tar))
+            return true;
         ans.pop_back();
-        
-        //ans.pop_back();
+
         return false;
     }
     string getDirections(TreeNode* root, int startValue, int destValue) {
@@ -32,19 +36,19 @@ public:
         preorder(root, src, startValue);
         preorder(root, dst, destValue);
         int i = 0;
-        while (i < src.size() && i < dst.size()){
-            if (src[i] == dst[i]){
-                i++; //j++;
-            }
-            else break;
+        int n = src.size();
+        int m = dst.size();
+        while (i < min(n, m)) {
+            if (src[i] == dst[i]) {
+                i++;
+            } else
+                break;
         }
-        //src = src.substr(i, src.size() - i);
         int len = i;
-        //i = 0;
-        while (i < src.size()){
+        while (i < n) {
             src[i] = 'U';
             i++;
         }
-        return src.substr(len, src.size()-len) + dst.substr(len, dst.size()-len);
+        return src.substr(len, n - len) + dst.substr(len, m - len);
     }
 };
