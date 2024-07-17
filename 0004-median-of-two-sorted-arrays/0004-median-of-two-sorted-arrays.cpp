3a7f45434041913638047e26127568d13258cce1
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-17 19:34:08 +0530

    Time: 21 ms (74.06%), Space: 94.8 MB (42.81%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -3,31 +3,33 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int m = nums1.size();
         int n = nums2.size();
-        if (m > n) return findMedianSortedArrays(nums2,nums1);
+        if (m > n)
+            return findMedianSortedArrays(nums2, nums1);
         int i = 0;
         int j = m;
-        while (i <= j){
-            int mid1 = i + (j-i)/2;
-            int mid2 = (m+n+1)/2 - mid1;
+        while (i <= j) {
+            int mid1 = i + (j - i) / 2;
+            int mid2 = (m + n + 1) / 2 - mid1;
             double l1 = INT_MIN, l2 = INT_MIN, r1 = INT_MAX, r2 = INT_MAX;
-            if (mid1 - 1 >= 0) l1 = nums1[mid1-1];
-            if (mid1 < m) r1 = nums1[mid1];
-            if (mid2 - 1 >= 0) l2 = nums2[mid2-1];
-            if (mid2 < n) r2 = nums2[mid2];
-            if (l1 > r2){
-                j = mid1-1;
-            }
-            else if (l2 > r1){
-                i = mid1+1;
-            }
-            else{
-                if ((m+n)%2) return max(l1,l2);
-                else return (max(l1,l2) + min(r1,r2))/2;
+            if (mid1 - 1 >= 0)
+                l1 = nums1[mid1 - 1];
+            if (mid1 < m)
+                r1 = nums1[mid1];
+            if (mid2 - 1 >= 0)
+                l2 = nums2[mid2 - 1];
+            if (mid2 < n)
+                r2 = nums2[mid2];
+            if (l1 > r2) {
+                j = mid1 - 1;
+            } else if (l2 > r1) {
+                i = mid1 + 1;
+            } else {
+                if ((m + n) % 2)
+                    return max(l1, l2);
+                else
+                    return (max(l1, l2) + min(r1, r2)) / 2;
             }
         }
-        //cout<< n ;
-        // if ((m+n)%2) return nums2[(m+n)/2];
-        // else return (double)(nums2[(m+n)/2] + nums2[(m+n)/2 -1])/2;
         return -1;
     }
 };
