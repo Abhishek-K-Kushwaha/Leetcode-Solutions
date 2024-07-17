27c40466339e1783759147c1436ae97e0aac08ae
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-17 19:32:52 +0530

    Time: 29 ms (36.59%), Space: 94.8 MB (46.71%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -3,47 +3,31 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int m = nums1.size();
         int n = nums2.size();
-        if (m + n == 1) {
-            if (m) return nums1[0];
-            return nums2[0];
-        }
+        if (m > n) return findMedianSortedArrays(nums2,nums1);
         int i = 0;
-        int j = 0;
-        double ans1 = 1e9;
-        double ans2 = 1e9;
-        while (i < m && j < n) {
-            if (nums1[i] < nums2[j]) {
-                i++;
-                if ((m + n + 1) / 2 == i + j) {
-                    ans1 = nums1[i - 1];
-                    break;
-                }
-            } else {
-                j++;
-                if ((m + n + 1) / 2 == i + j) {
-                    ans1 = nums2[j - 1];
-                    break;
-                }
+        int j = m;
+        while (i <= j){
+            int mid1 = i + (j-i)/2;
+            int mid2 = (m+n+1)/2 - mid1;
+            double l1 = INT_MIN, l2 = INT_MIN, r1 = INT_MAX, r2 = INT_MAX;
+            if (mid1 - 1 >= 0) l1 = nums1[mid1-1];
+            if (mid1 < m) r1 = nums1[mid1];
+            if (mid2 - 1 >= 0) l2 = nums2[mid2-1];
+            if (mid2 < n) r2 = nums2[mid2];
+            if (l1 > r2){
+                j = mid1-1;
+            }
+            else if (l2 > r1){
+                i = mid1+1;
+            }
+            else{
+                if ((m+n)%2) return max(l1,l2);
+                else return (max(l1,l2) + min(r1,r2))/2;
             }
         }
-
-        if (j >= n) {
-            if (ans1 == 1e9)
-                ans1 = nums1[(m + n + 1) / 2 - j - 1];
-            ans2 = nums1[(m + n + 1) / 2 - j];
-        } else if (i >= m) {
-            if (ans1 == 1e9)
-                ans1 = nums2[(m + n + 1) / 2 - i - 1];
-            ans2 = nums2[(m + n + 1) / 2 - i];
-        }
-
-        if ((m + n) % 2)
-            return ans1;
-        if (ans2 != 1e9)
-            return (ans1 + ans2) / 2;
-        else {
-            ans2 = min(nums1[i], nums2[j]);
-            return (ans1 + ans2) / 2;
-        }
+        //cout<< n ;
+        // if ((m+n)%2) return nums2[(m+n)/2];
+        // else return (double)(nums2[(m+n)/2] + nums2[(m+n)/2 -1])/2;
+        return -1;
     }
 };
