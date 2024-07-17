840e39ba23ddc4924547bb527c7517cc8e34db05
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-17 12:21:08 +0530

    Time: 14 ms (70.59%), Space: 27.2 MB (21.35%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -6,33 +6,41 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    void postorder(TreeNode* node, unordered_set<int>& del, vector<TreeNode*>& ans){
-        if (node == nullptr) return;
+    void postorder(TreeNode* node, unordered_set<int>& del,
+                   vector<TreeNode*>& ans) {
+        if (node == nullptr)
+            return;
         TreeNode* l = node->left;
         TreeNode* r = node->right;
         postorder(l, del, ans);
         postorder(r, del, ans);
-        if (l && (del.find(l->val) != del.end())) node->left = nullptr;
-        if (r && (del.find(r->val) != del.end())) node->right = nullptr;
-        if (del.find(node->val) != del.end()){
-            if (node->left) ans.push_back(node->left);
-            if (node->right) ans.push_back(node->right);
+        if (l && (del.find(l->val) != del.end()))
+            node->left = nullptr;
+        if (r && (del.find(r->val) != del.end()))
+            node->right = nullptr;
+        if (del.find(node->val) != del.end()) {
+            if (node->left)
+                ans.push_back(node->left);
+            if (node->right)
+                ans.push_back(node->right);
         }
     }
 
     vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
         unordered_set<int> del;
-        for (auto i: to_delete){
+        for (auto i : to_delete) {
             del.insert(i);
         }
         vector<TreeNode*> ans;
         postorder(root, del, ans);
-        if (del.find(root->val) == del.end()) ans.push_back(root);
+        if (del.find(root->val) == del.end())
+            ans.push_back(root);
         return ans;
     }
 };
