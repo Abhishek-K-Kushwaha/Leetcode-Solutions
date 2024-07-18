da01df33c161a7d2d8d2fbd630ab520588093c82
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-18 12:15:29 +0530

    Time: 167 ms (16.03%), Space: 56.9 MB (14.29%) - LeetHub

--- 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.cpp
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.cpp
@@ -13,9 +13,6 @@ class Solution {
 public:
     unordered_map<TreeNode*,TreeNode*> parent;
     vector<TreeNode*> leafs;
-    unordered_set<TreeNode*> vis; 
-    int ans = 0;
-    int range;
     bool inorder(TreeNode* node){
         if (node == nullptr) return false;
         if (!node->left && !node->right) leafs.push_back(node);
@@ -24,20 +21,22 @@ public:
         return true;
     }
 
-    void dfs(TreeNode* node, int dist){
+    void dfs(TreeNode* node, unordered_set<TreeNode*>& vis, int dist, int& range, int& ans){
         if (dist > range || node == nullptr) return;
         vis.insert(node);
         if (dist && !node->left && !node->right) ans++;
-        if (vis.find(node->left) == vis.end()) dfs(node->left, dist+1);
-        if (vis.find(node->right) == vis.end()) dfs(node->right, dist+1);     
-        if (vis.find(parent[node]) == vis.end()) dfs(parent[node], dist+1);   
+        if (vis.find(node->left) == vis.end()) dfs(node->left, vis, dist+1, range, ans);
+        if (vis.find(node->right) == vis.end()) dfs(node->right, vis, dist+1, range, ans);     
+        if (vis.find(parent[node]) == vis.end()) dfs(parent[node], vis, dist+1, range, ans);   
     }
 
     int countPairs(TreeNode* root, int distance) {
         inorder(root);
-        range = distance;
+        unordered_set<TreeNode*> vis; 
+        int ans = 0;
         for (auto it:leafs){
-            dfs(it, 0);
+            //vis.insert(it);
+            dfs(it, vis, 0, distance, ans);
             vis.clear();            
         }
         return ans/2;
