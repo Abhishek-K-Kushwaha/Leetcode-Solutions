49c677cc42b8f8cd89b1f8cc8e7733d3bcc0fa5a
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-18 13:27:15 +0530

    Time: 37 ms (91.43%), Space: 32.1 MB (92.86%) - LeetHub

--- 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.cpp
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.cpp
@@ -6,37 +6,33 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     int range;
     int ans = 0;
-    vector<int> postorder(TreeNode* node){
-        if (node == nullptr) return vector<int>(11,0);
-        if (!node->left && !node->right){
-            vector<int> leafs(11,0);
+    vector<int> postorder(TreeNode* node) {
+        if (node == nullptr)
+            return vector<int>(11, 0);
+        if (!node->left && !node->right) {
+            vector<int> leafs(11, 0);
             leafs[0] = 1;
             return leafs;
         }
         vector<int> l = postorder(node->left);
-        //cout<< l.size() <<" ";
         vector<int> r = postorder(node->right);
-        //cout<< r.size()<<" ";
 
-        for (int i = 0; i <= range; i++){
-            for (int j = 0; j <= (range - i -2); j++){
-                ans += l[i]*r[j];
-                //cout<< l[i] << " " << r[j];
+        for (int i = 0; i <= range; i++) {
+            for (int j = 0; j <= (range - i - 2); j++) {
+                ans += l[i] * r[j];
             }
         }
-        //vector<int> curr(11,0);
-        for (int i = 9; i >= 0; i--){
-            l[i+1] = l[i] + r[i];
-            //cout<< curr[i+1] << " ";
+        for (int i = 9; i >= 0; i--) {
+            l[i + 1] = l[i] + r[i];
         }
-        //cout<< "\n";
         l[0] = 0;
         return l;
     }
