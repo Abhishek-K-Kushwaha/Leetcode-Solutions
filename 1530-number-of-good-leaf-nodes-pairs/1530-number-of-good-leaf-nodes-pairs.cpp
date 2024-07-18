c519d6731dda3a4f2eb8905a66b3b9fb04532fe0
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-18 13:26:58 +0530

    Time: 34 ms (96.03%), Space: 31.9 MB (93.81%) - LeetHub

--- 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.cpp
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.cpp
@@ -1,37 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int countPairs(TreeNode* root, int distance) {
-        return postOrder(root, distance)[11];
-    }
-    
-private:
-    vector<int> postOrder(TreeNode* node, int distance) {
-        if (!node) return vector<int>(12, 0);
-        if (!node->left && !node->right) {
-            vector<int> current(12, 0);
-            current[0] = 1;
-            return current;
+    int range;
+    int ans = 0;
+    vector<int> postorder(TreeNode* node){
+        if (node == nullptr) return vector<int>(11,0);
+        if (!node->left && !node->right){
+            vector<int> leafs(11,0);
+            leafs[0] = 1;
+            return leafs;
         }
-        
-        vector<int> left = postOrder(node->left, distance);
-        vector<int> right = postOrder(node->right, distance);
-        
-        vector<int> current(12, 0);
-        
-        for (int i = 0; i < 10; i++) {
-            current[i + 1] += left[i] + right[i];
-        }
-        
-        current[11] += left[11] + right[11];
-        
-        for (int d1 = 0; d1 <= distance; d1++) {
-            for (int d2 = 0; d2 <= distance; d2++) {
-                if (2 + d1 + d2 <= distance) {
-                    current[11] += left[d1] * right[d2];
-                }
+        vector<int> l = postorder(node->left);
+        //cout<< l.size() <<" ";
+        vector<int> r = postorder(node->right);
+        //cout<< r.size()<<" ";
+
+        for (int i = 0; i <= range; i++){
+            for (int j = 0; j <= (range - i -2); j++){
+                ans += l[i]*r[j];
+                //cout<< l[i] << " " << r[j];
             }
         }
-        
-        return current;
+        //vector<int> curr(11,0);
+        for (int i = 9; i >= 0; i--){
+            l[i+1] = l[i] + r[i];
+            //cout<< curr[i+1] << " ";
+        }
+        //cout<< "\n";
+        l[0] = 0;
+        return l;
+    }
+    int countPairs(TreeNode* root, int distance) {
+        range = distance;
+        postorder(root);
+        return ans;
     }
 };
