1f9022068ac076e1042d71d88f0ccd8003894014
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-19 13:28:15 +0530

    Time: 20 ms (23.27%), Space: 14.3 MB (27.66%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
@@ -3,24 +3,38 @@ public:
     vector<int> luckyNumbers (vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<int> rowdata(m, INT_MAX);
-        vector<int> coldata(n, INT_MIN);
-
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
-                rowdata[i] = min(rowdata[i], matrix[i][j]);
-                coldata[j] = max(coldata[j], matrix[i][j]);
-            }
-        }
+        // vector<int> rowdata(m, INT_MAX);
+        // vector<int> coldata(n, INT_MIN);
+        
         vector<int> ans;
         for (int i = 0; i < m; i++){
+            int minele = INT_MAX;
+            pair<int,int> cord;
             for (int j = 0; j < n; j++){
-                if (matrix[i][j] == rowdata[i] && matrix[i][j] == coldata[j]){ 
-                    ans.push_back(matrix[i][j]);
+                if (minele > matrix[i][j]){
+                    minele = matrix[i][j];
+                    cord = {i,j};
+                }
+            }
+            int flag = 1;
+            for (int row = 0; row < m; row++){
+                if (minele < matrix[row][cord.second]){ 
+                    flag = 0;
                     break;
                 }
             }
+            if (flag) ans.push_back(matrix[cord.first][cord.second]);
+
         }
+        
+        // for (int i = 0; i < m; i++){
+        //     for (int j = 0; j < n; j++){
+        //         if (matrix[i][j] == rowdata[i] && matrix[i][j] == coldata[j]){ 
+        //             ans.push_back(matrix[i][j]);
+        //             break;
+        //         }
+        //     }
+        // }
         return ans;
     }
 };
