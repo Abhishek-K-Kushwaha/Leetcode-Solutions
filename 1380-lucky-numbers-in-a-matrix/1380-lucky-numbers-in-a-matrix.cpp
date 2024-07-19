36ba47659b4a4fc92aa9c9c2caa76b3ecea966ce
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-19 14:08:40 +0530

    Time: 11 ms (90.82%), Space: 14.1 MB (43.88%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    vector<int> luckyNumbers (vector<vector<int>>& matrix) {
+    vector<int> luckyNumbers(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
         vector<int> ans;
-        for (int i = 0; i < m; i++){
+        for (int i = 0; i < m; i++) {
             int minele = INT_MAX;
-            pair<int,int> cord;
-            for (int j = 0; j < n; j++){
-                if (minele > matrix[i][j]){
+            pair<int, int> cord;
+            for (int j = 0; j < n; j++) {
+                if (minele > matrix[i][j]) {
                     minele = matrix[i][j];
-                    cord = {i,j};
+                    cord = {i, j};
                 }
             }
             int flag = 1;
-            for (int row = 0; row < m; row++){
-                if (minele < matrix[row][cord.second]){ 
+            for (int row = 0; row < m; row++) {
+                if (minele < matrix[row][cord.second]) {
                     flag = 0;
                     break;
                 }
             }
-            if (flag){ 
+            if (flag) {
                 ans.push_back(matrix[cord.first][cord.second]);
                 break;
             }
