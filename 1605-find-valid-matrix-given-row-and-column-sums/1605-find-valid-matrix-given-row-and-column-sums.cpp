b6553f5d47477b3026d0ff5fa10b8556b09cec74
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-20 20:03:22 +0530

    Time: 29 ms (89.39%), Space: 35.9 MB (27.54%) - LeetHub

--- 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.cpp
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.cpp
@@ -6,6 +6,16 @@ public:
         vector<vector<int>> ans(m, vector<int>(n,0));
         int r = 0, c = 0;
         while (r < m || c < n){
+            if (r == m){
+                ans[r-1][c] = colSum[c];
+                c++;
+                continue;
+            }
+            if (c == n){
+                ans[r][c-1] = rowSum[r];
+                r++;
+                continue;
+            }
             int val = min(rowSum[r], colSum[c]);
             rowSum[r] -= val;
             colSum[c] -= val;
