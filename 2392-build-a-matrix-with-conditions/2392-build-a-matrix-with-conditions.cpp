046e22f83bb24f649aabcdf114fa9eb0e08e06bd
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-21 21:12:38 +0530

    Time: 78 ms (98.03%), Space: 67.8 MB (13.24%) - LeetHub

--- 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
@@ -1,17 +1,18 @@
 class Solution {
 private:
     int counter;
-    bool dfs(int node, bool iscol, vector<bool>& vis, vector<vector<int>>& cords, vector<unordered_set<int>>& adj, vector<bool>& path){ 
+    bool dfs(int node, bool iscol, vector<bool>& vis, vector<int>& cords, vector<unordered_set<int>>& adj, vector<bool>& path, vector<vector<int>>& ans){ 
         if (path[node]) return true;
         if (vis[node]) return false;
         vis[node] = true;
         path[node] = true;
 
         for (auto nxt : adj[node]){
-            if (dfs(nxt, iscol, vis, cords, adj, path)) return true;
+            if (dfs(nxt, iscol, vis, cords, adj, path, ans)) return true;
         }
-        
-        cords[node][iscol] = counter;
+
+        if (iscol) ans[cords[node]][counter] = node;
+        else cords[node] = counter;
         counter--;
         path[node] = false;
         return false;
@@ -22,7 +23,7 @@ public:
         vector<vector<int>> ans(k, vector<int>(k, 0));
         vector<unordered_set<int>> adj(k+1);
         vector<bool> vis(k+1, 0);
-        vector<vector<int>> cords(k+1, vector<int>(2, -1)); // {i,j} pairs for all nums 1 to k
+        vector<int> cords(k+1); // stores rows for all nums 1 to k
         counter = k-1;
 
         for (auto& it: rowConditions){
@@ -31,7 +32,7 @@ public:
 
         vector<bool> path(k+1, 0);
         for (int node = 1; node <= k; node++){
-            if (dfs(node, 0, vis, cords, adj, path)) return {}; // for getting rows
+            if (dfs(node, 0, vis, cords, adj, path, ans)) return {}; // for getting rows
         }
 
         counter = k-1;
@@ -45,14 +46,14 @@ public:
         }
         
         for (int node = 1; node <= k; node++){
-            if (dfs(node, 1, vis, cords, adj, path)) return {};// for getting cols
+            if (dfs(node, 1, vis, cords, adj, path, ans)) return {};// for getting cols
         }
 
-        for (int i = 1; i <= k; i++){
-            int r = cords[i][0];
-            int c = cords[i][1];
-            ans[r][c] = i;
-        }
+        // for (int i = 1; i <= k; i++){
+        //     int r = cords[i][0];
+        //     int c = cords[i][1];
+        //     ans[r][c] = i;
+        // }
         return ans;        
     }
 };
