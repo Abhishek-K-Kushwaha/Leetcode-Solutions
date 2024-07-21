497d1bf203df23788b64367c28347e43ac41c875
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-21 20:58:19 +0530

    Time: 113 ms (29.86%), Space: 70.1 MB (6.48%) - LeetHub

--- 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
@@ -2,7 +2,6 @@ class Solution {
 private:
     int counter;
     bool dfs(int node, bool iscol, vector<bool>& vis, vector<vector<int>>& cords, vector<unordered_set<int>>& adj, vector<bool>& path){ 
-        //cout << node << " ";
         if (path[node]) return true;
         if (vis[node]) return false;
         vis[node] = true;
@@ -10,11 +9,9 @@ private:
 
         if (!adj[node].empty()){
             for (auto nxt : adj[node]){
-                //cout << "nxt=" << nxt<<" ";
                 if (dfs(nxt, iscol, vis, cords, adj, path)) return true;
             }
         }
-        //cout << node << " ";
         cords[node][iscol] = counter;
         counter--;
         path[node] = false;
@@ -33,23 +30,10 @@ public:
             adj[it[0]].insert(it[1]);
         }
 
-        // for (int i = 1; i <= k; i++){
-        //     cout<< i << " : ";
-        //     for (auto it:adj[i]){
-        //         cout<< it << " ";
-        //     }
-        //     cout<< "\n";
-        // }
-        //cout<< (adj[2].find(2) == !)
-        //cout<< "row start\n";
         for (int node = 1; node <= k; node++){
             vector<bool> path(k+1, 0);
             if (dfs(node, 0, vis, cords, adj, path)) return {}; // for getting rows
-            //cout<< "\n";
         }
-
-
-        //cout<< "row compl" ;
         counter = k-1;
         vector<unordered_set<int>> adjcol(k+1);
         vector<bool> viscol(k+1, 0);
@@ -61,7 +45,6 @@ public:
         for (int node = 1; node <= k; node++){
             vector<bool> path(k+1, 0);
             if (dfs(node, 1, viscol, cords, adjcol, path)) return {};// for getting cols
-            //cout<< "\n";
         }
 
         for (int i = 1; i <= k; i++){
