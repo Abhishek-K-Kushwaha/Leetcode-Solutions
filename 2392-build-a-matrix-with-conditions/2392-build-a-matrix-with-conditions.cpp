c217bcc7f6e73c52836730e951eb939fbc766e98
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-21 21:39:53 +0530

    Time: 92 ms (81.41%), Space: 63.9 MB (61.13%) - LeetHub

--- 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
@@ -1,7 +1,7 @@
 class Solution {
 private:
     int counter;
-    bool dfs(int node, bool iscol, vector<bool>& vis, vector<int>& cords, vector<unordered_set<int>>& adj, vector<bool>& path, vector<vector<int>>& ans){ 
+    bool dfs(int node, bool iscol, vector<bool>& vis, vector<int>& cords, vector<vector<int>>& adj, vector<bool>& path, vector<vector<int>>& ans){ 
         if (path[node]) return true;
         if (vis[node]) return false;
         vis[node] = true;
@@ -21,32 +21,33 @@ private:
 public:
     vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
         vector<vector<int>> ans(k, vector<int>(k, 0));
-        vector<unordered_set<int>> adj(k+1);
+        vector<vector<int>> adj_row(k+1), adj_col(k+1);
         vector<bool> vis(k+1, 0);
         vector<int> cords(k+1); // stores rows for all nums 1 to k
-        counter = k-1;
-
-        for (auto& it: rowConditions){
-            adj[it[0]].insert(it[1]);
-        }
-
         vector<bool> path(k+1, 0);
-        for (int node = 1; node <= k; node++){
-            if (dfs(node, 0, vis, cords, adj, path, ans)) return {}; // for getting rows
+        
+        // Build adjacency list for row conditions
+        for (auto& it: rowConditions){
+            adj_row[it[0]].push_back(it[1]);
         }
 
         counter = k-1;
-        for (int i = 1; i <=k; i++){
-            vis[i] = false;
-            adj[i].clear();
+        for (int node = 1; node <= k; node++){
+            if (dfs(node, false, vis, cords, adj_row, path, ans)) return {}; // for getting rows
         }
 
+        // Reset for column conditions
+        fill(vis.begin(), vis.end(), false);
+        fill(path.begin(), path.end(), false);
+        counter = k-1;
+
+        // Build adjacency list for column conditions
         for (auto& it: colConditions){
-            adj[it[0]].insert(it[1]);
+            adj_col[it[0]].push_back(it[1]);
         }
         
         for (int node = 1; node <= k; node++){
-            if (dfs(node, 1, vis, cords, adj, path, ans)) return {}; // for getting cols
+            if (dfs(node, true, vis, cords, adj_col, path, ans)) return {}; // for getting cols
         }
 
         return ans;        
