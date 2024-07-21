2a5dd4ff0238572f2b9ff5ceb51f951677cce205
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-21 21:44:35 +0530

    Time: 78 ms (98.03%), Space: 67.8 MB (13.24%) - LeetHub

--- 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
@@ -1,67 +1,59 @@
 class Solution {
+private:
+    int counter;
+    bool dfs(int node, bool iscol, vector<bool>& vis, vector<int>& cords, vector<unordered_set<int>>& adj, vector<bool>& path, vector<vector<int>>& ans){ 
+        if (path[node]) return true;
+        if (vis[node]) return false;
+        vis[node] = true;
+        path[node] = true;
+
+        for (auto nxt : adj[node]){
+            if (dfs(nxt, iscol, vis, cords, adj, path, ans)) return true;
+        }
+
+        if (iscol) ans[cords[node]][counter] = node;
+        else cords[node] = counter;
+        counter--;
+        path[node] = false;
+        return false;
+    }
+
 public:
     vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
-        unordered_map<int, vector<int>> graph;
-        for (const auto& edge : rowConditions) {
-            graph[edge[0]].push_back(edge[1]);
+        vector<vector<int>> ans(k, vector<int>(k, 0));
+        vector<unordered_set<int>> adj(k+1);
+        vector<bool> vis(k+1, 0);
+        vector<int> cords(k+1); // stores rows for all nums 1 to k
+        counter = k-1;
+
+        for (auto& it: rowConditions){
+            adj[it[0]].insert(it[1]);
         }
 
-        vector<int> row_sorting = topo_sort(graph, k);
-        graph.clear();
-        for (const auto& edge : colConditions) {
-            graph[edge[0]].push_back(edge[1]);
-        }
-        vector<int> col_sorting = topo_sort(graph, k);
-
-        if (row_sorting.empty() || col_sorting.empty()) {
-            return {};
+        vector<bool> path(k+1, 0);
+        for (int node = 1; node <= k; node++){
+            if (dfs(node, 0, vis, cords, adj, path, ans)) return {}; // for getting rows
         }
 
-        unordered_map<int, pair<int, int>> value_position;
-        for (int i = 0; i < k; ++i) {
-            value_position[row_sorting[i]].first = i;
-            value_position[col_sorting[i]].second = i;
+        counter = k-1;
+        for (int i = 1; i <=k; i++){
+            vis[i] = false;
+            adj[i].clear();
         }
 
-        vector<vector<int>> res(k, vector<int>(k, 0));
-        for (int value = 1; value <= k; ++value) {
-            int row = value_position[value].first;
-            int column = value_position[value].second;
-            res[row][column] = value;
+        for (auto& it: colConditions){
+            adj[it[0]].insert(it[1]);
+        }
+        
+        for (int node = 1; node <= k; node++){
+            if (dfs(node, 1, vis, cords, adj, path, ans)) return {};// for getting cols
         }
 
-        return res;
-    }
-
-private:
-    bool dfs(int src, unordered_map<int, vector<int>>& graph, unordered_set<int>& visited, unordered_set<int>& cur_path, vector<int>& res) {
-        if (cur_path.count(src)) return false;  // cycle detected
-        if (visited.count(src)) return true;  // all okay, but we've already visited this node
-
-        visited.insert(src);
-        cur_path.insert(src);
-
-        for (int neighbor : graph[src]) {
-            if (!dfs(neighbor, graph, visited, cur_path, res))  // if any child returns false
-                return false;
-        }
-
-        cur_path.erase(src);  // backtrack path
-        res.push_back(src);
-        return true;
-    }
-
-    vector<int> topo_sort(unordered_map<int, vector<int>>& graph, int k) {
-        unordered_set<int> visited;
-        unordered_set<int> cur_path;
-        vector<int> res;
-
-        for (int src = 1; src <= k; ++src) {
-            if (!dfs(src, graph, visited, cur_path, res))
-                return {};
-        }
-
-        reverse(res.begin(), res.end());  // we will have res as reversed so we need to reverse it one more time
-        return res;
+        // for (int i = 1; i <= k; i++){
+        //     int r = cords[i][0];
+        //     int c = cords[i][1];
+        //     ans[r][c] = i;
+        // }
+        return ans;        
     }
 };
