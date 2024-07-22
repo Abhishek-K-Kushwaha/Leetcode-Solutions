3f666fbb0ff7cba341724560de909107e6672e4d
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-22 11:27:03 +0530

    Time: 27 ms (70.61%), Space: 26.5 MB (60.58%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        //vector<string> ans;
         map<int,string> dict;
         int n = names.size();
         for (int i = 0; i < n; i++){
