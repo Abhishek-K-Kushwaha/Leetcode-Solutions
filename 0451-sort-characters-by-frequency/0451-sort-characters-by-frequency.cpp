0d238b05abe309fcc5841d322a17d2e6cb115d0e
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-23 15:20:15 +0530

    Time: 22 ms (6.39%), Space: 10.5 MB (83.39%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -5,7 +5,6 @@ public:
         for (auto i : s) {
             freq[i - '0']++;
         }
-        // cout << freq['a'-'A'] << " " << freq['k'-'A'];
         sort(s.begin(), s.end(), [&](char a, char b) {
             if (freq[a - '0'] != freq[b - '0'])
                 return freq[a - '0'] > freq[b - '0'];
