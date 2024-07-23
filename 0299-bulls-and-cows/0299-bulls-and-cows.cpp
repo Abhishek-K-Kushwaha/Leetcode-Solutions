8c964457f05c052033e39aea0786632d2c835fef
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-23 20:00:17 +0530

    Time: 0 ms (100%), Space: 9.5 MB (15.17%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.cpp
+++ 0299-bulls-and-cows/0299-bulls-and-cows.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     string getHint(string secret, string guess) {
-        unordered_map<char,pair<int,int>> map; //secret, guess
-        //unordered_map<char,int> gmap;
+        unordered_map<char,int> smap;
+        unordered_map<char,int> gmap;
         int n = guess.size();
         int bulls = 0;
         for (int i = 0; i < n; i++){
@@ -10,13 +10,13 @@ public:
                 bulls++;
             }
             else{
-                map[secret[i]].first++;
-                map[guess[i]].second++;
+                smap[secret[i]]++;
+                gmap[guess[i]]++;
             }
         }
         int cows = 0;
-        for (auto& it:map){
-            cows += min(it.second.second, it.second.first);
+        for (auto& it:smap){
+            cows += min(it.second, gmap[it.first]);
         }
         return to_string(bulls) + "A" + to_string(cows) + "B";
     }
