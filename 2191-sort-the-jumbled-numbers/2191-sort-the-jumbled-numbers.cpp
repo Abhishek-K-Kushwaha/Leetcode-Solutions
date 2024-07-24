c714df7d2aed450a0e9c1e859635b183f885e3f1
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-24 13:42:26 +0530

    Time: 1095 ms (8.66%), Space: 107.1 MB (33.77%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
@@ -6,7 +6,7 @@ public:
         for (int num:nums){
             int org = num;
             int val = 0;
-            int i = 0;
+            int i = 1;
             if (org == 0){
                 vals[org] = {ind,(mapping[0])};
                 ind++;
@@ -15,8 +15,8 @@ public:
             while(num){
                 int r = num % 10;
                 num /= 10;
-                val += mapping[r] * pow(10,i);
-                i++;
+                val += mapping[r] * i;
+                i *= 10;
             }
             vals[org] = {ind, val};
             ind++;
