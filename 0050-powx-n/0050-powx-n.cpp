20c2f707bd0197e278de2ebf6b9217b617091914
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-24 18:39:02 +0530

    Time: 0 ms (100%), Space: 8.6 MB (12.2%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+private:
+    vector<double> dp = vector<double>(33, 0);
+    
+    double f(double x, long long n) {
+        if (n == 0) return 1;
+        if (x == 0 || n == 1) return x;
+        long long m = 1;
+        int i = 0;
+        double ans = x;
+        while (m * 2 <= n) {
+            m *= 2;
+            i++;
+            if (dp[i] != 0) ans = dp[i];
+            else {
+                ans *= ans;
+                dp[i] = ans;
+            }
+        }
+        return ans * myPow(x, n - m);
+    }
+
+public:
+    double myPow(double x, int n) {
+        if (n == 0) return 1;
+        if (x == 0) return 0;
+        if (n == INT_MIN) {
+            return 1 / (f(x, (long long)INT_MAX) * x); // Special case for INT_MIN
+        }
+        if (n >= 0) return f(x, n);
+        else return 1 / f(x, (long long)-n);
+    }
+};
