1fa843171af2d66427a362d6e554f70cfdfecd43
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-24 13:52:52 +0530

    Time: 643 ms (19.48%), Space: 100.7 MB (36.36%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
-        unordered_map<int, pair<int,int>> vals; // orgnum -> {ind, val}
-        int ind = 0;
+        unordered_map<int, int> vals; // orgnum -> {ind, val}
         for (int num:nums){
             int org = num;
             int val = 0;
             int i = 1;
             if (org == 0){
-                vals[org] = {ind,(mapping[0])};
-                ind++;
+                vals[org] = mapping[0];
                 continue;
             }
             while(num){
@@ -18,14 +16,10 @@ public:
                 val += mapping[r] * i;
                 i *= 10;
             }
-            vals[org] = {ind, val};
-            ind++;
+            vals[org] = val;
         }
         sort(nums.begin(), nums.end(), [&](int a, int b){
-            //int inda = vals[a].first; //find(nums.begin(), nums.end(), a) - nums.begin();
-            //int indb = vals[b].first; //find(nums.begin(), nums.end(), b) - nums.begin();
-            //if (vals[a].second == vals[b].second) return inda < indb;
-            return vals[a].second < vals[b].second;
+            return vals[a] < vals[b];
         });
         // for (auto it: vals){
         //     cout << it << " ";
