ae44ea8816b9a871bad9e0d7f671bbf997575ba7
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-25 19:51:55 +0530

    Time: 2 ms (78.52%), Space: 13.6 MB (22.6%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -6,15 +6,9 @@ public:
         int i = 1;
         int cnt = 1;
         for (int j = 1; j < n; j++){
-            if (nums[j] == nums[j-1]){
-                cnt++;
-                if (cnt <= 2){
-                    nums[i] = nums[j];
-                    i++;
-                }
-            }
-            else{
-                cnt = 1;
+            if (nums[j] == nums[j-1]) cnt++;
+            else cnt = 1;
+            if (cnt <= 2){
                 nums[i] = nums[j];
                 i++;
             }
