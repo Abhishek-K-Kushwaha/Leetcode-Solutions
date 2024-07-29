352be2e44445f343fe39c3c271a22e8cc6314f51
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-07-29 15:13:29 +0530

    Time: 30 ms (70.71%), Space: 11.6 MB (51.74%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -1,48 +1,36 @@
 class Solution {
 public:
     int numTeams(vector<int>& rating) {
+        
         int n = rating.size();
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(n+1, vector<int>(4, 0)));
-        for (int i = 0; i <= n; i++){
-            for (int last = 0; last <= n; last++){
-                dp[i][last][3] = 1;
-            }
-        }
-        for (int i = n-1; i >= 0; i--){
-            for (int last = i-1; last >= -1; last--){
-                for (int cnt = 2; cnt >= 0; cnt--){
-                    int pick = 0;
-                    if (last < 0 || rating[i] > rating[last])
-                        pick = dp[i+1][i+1][cnt+1]; //f(i + 1, cnt + 1, i, rating, dp);
-                    int notpick = dp[i+1][last+1][cnt]; //f(i + 1, cnt, last, rating, dp);
-                    dp[i][last+1][cnt] = pick + notpick;
-                }
-            }
-        }
-        int cnt1 = dp[0][0][0]; //f(0, 0, -1, rating, dp);
-        //cout << cnt1 << " ";
-        vector<vector<int>> temp(n+1, vector<int>(4, 0));
-        fill(dp.begin(), dp.end(), temp);
-        reverse(rating.begin(), rating.end());
+        int count = 0;
 
-        for (int i = 0; i <= n; i++){
-            for (int last = 0; last <= n; last++){
-                dp[i][last][3] = 1;
-            }
-        }
-        for (int i = n-1; i >= 0; i--){
-            for (int last = i-1; last >= -1; last--){
-                for (int cnt = 2; cnt >= 0; cnt--){
-                    int pick = 0;
-                    if (last < 0 || rating[i] > rating[last])
-                        pick = dp[i+1][i+1][cnt+1]; //f(i + 1, cnt + 1, i, rating, dp);
-                    int notpick = dp[i+1][last+1][cnt]; //f(i + 1, cnt, last, rating, dp);
-                    dp[i][last+1][cnt] = pick + notpick;
+        for(int i=0; i<n; i++){
+
+            int leftsmaller = 0, leftgreater = 0;
+            int rightsmaller = 0, rightgreater = 0;
+
+            for(int j=0; j<i ; j++){
+                if(rating[j] > rating[i]){
+                    leftgreater++;
+                }
+                if(rating[j] < rating[i]){
+                    leftsmaller++;
                 }
             }
+
+            for(int j=i+1; j<n ; j++){
+                if(rating[j] > rating[i]){
+                    rightgreater++;
+                }
+                if(rating[j] < rating[i]){
+                    rightsmaller++;
+                }
+            }
+
+            count += leftsmaller*rightgreater + leftgreater*rightsmaller;
         }
-        int cnt2 = dp[0][0][0]; //f(0, 0, -1, rating, dp);
-        //cout << cnt2;
-        return cnt1 + cnt2;
+
+        return count;
     }
 };
