5f4e7a7eb45d326020babaf16dc3b4230f27b0c9
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-08-01 16:35:49 +0530

    Time: 4 ms (82.75%), Space: 17.7 MB (16.71%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -3,8 +3,12 @@ public:
     int countSeniors(vector<string>& details) {
         int ans = 0;
         for (auto& it:details){
-            string age = it.substr(11,2);
-            if (stoi(age) > 60) ans++;
+            // string age = it.substr(11,2);
+            int age = 0;
+            for (int i = 11; i < 13; i++){
+                age = age*10 + (it[i]-'0');
+            }
+            if (age > 60) ans++;
         }
         return ans;
     }
