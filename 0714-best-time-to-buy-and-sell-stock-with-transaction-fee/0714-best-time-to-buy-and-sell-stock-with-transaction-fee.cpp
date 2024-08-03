65230170005a5492b569e037dd73a60c5a555df9
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-08-03 17:27:03 +0530

    Time: 156 ms (41.66%), Space: 93.5 MB (45.5%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,20 +1,41 @@
 class Solution {
 public:
+    int f(int i, int own, vector<int>& prices, int fee, vector<vector<int>>& dp){
+        if (i == prices.size()) return 0;
+        if (dp[i][own] != -1) return dp[i][own];
+        int profit = 0;
+        if (own){
+            int sell = prices[i] - fee + f(i+1, 0, prices, fee, dp);
+            int notsell = f(i+1, 1, prices, fee, dp);
+            profit = max({sell, notsell, profit});
+        }
+        else{
+            int buy = -prices[i] + f(i+1, 1, prices, fee, dp);
+            int notbuy = f(i+1, 0, prices, fee, dp);
+            profit = max({buy , notbuy, profit});
+        }
+        return dp[i][own] = profit;
+
+    }
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
-        vector<int> prev(2,0), nxt(2);
-        for (int ind = n-1; ind >= 0; ind--){
-            //if (canbuy == 1) {
-            int buy = -prices[ind] + prev[0]; 
-            int notbuy = prev[1]; 
-            nxt[1] = max(buy, notbuy);
-
-            // else if (canbuy == 0)
-            int sell = prices[ind] - fee + prev[1]; 
-            int notsell = prev[0]; 
-            nxt[0] = max(sell, notsell);
-            prev = nxt;
+        vector<vector<int>> dp(n+1, vector<int>(2, 0));
+        for (int i = n-1; i >=0; i--){
+            for (int own = 0; own <= 1; own++){
+                int profit = 0;
+                if (own){
+                    int sell = prices[i] - fee + dp[i+1][0]; //f(i+1, 0, prices, fee, dp);
+                    int notsell = dp[i+1][1]; //f(i+1, 1, prices, fee, dp);
+                    profit = max({sell, notsell, profit});
+                }
+                else{
+                    int buy = -prices[i] + dp[i+1][1]; //f(i+1, 1, prices, fee, dp);
+                    int notbuy = dp[i+1][0]; //f(i+1, 0, prices, fee, dp);
+                    profit = max({buy , notbuy, profit});
+                }
+                dp[i][own] = profit;
+            }
         }
-        return prev[1];
+        return dp[0][0]; //f(0, 0, prices, fee, dp);
     }
 };
