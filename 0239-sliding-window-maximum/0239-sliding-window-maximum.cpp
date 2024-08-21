c811e1618f585f81d3bddf9facbad4fcaffc1c69
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-08-21 23:08:24 +0530

    Time: 148 ms (95.27%), Space: 132.5 MB (98.56%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -5,7 +5,6 @@ public:
         vector<int> ans(n-k+1, -1);
         deque<int> dq;
         for (int i = 0; i < k; i++){
-            //if (dq.empty()) dq.push_back(nums[i]);
             while (!dq.empty() && dq.back() < nums[i]){
                 dq.pop_back();
             }
