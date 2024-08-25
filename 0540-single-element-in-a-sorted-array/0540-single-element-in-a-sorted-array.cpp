3bca8824b23429b348eb1c23ab9e50a605258ef4
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-08-25 17:07:55 +0530

    Time: 21 ms (39.84%), Space: 25 MB (41.11%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int n = nums.size();
-        int i = 0, j = (n-1)/2;
-        int ans = -1;
-        while (i <= j){
-            int mid = (i+j)/2;
-            int ind = 2*mid;
-            if (ind == n-1){
-                return nums[ind];
+        
+        int l = 0;
+        int h  = nums.size() - 1;
+        
+        
+        while(l<h){
+            int m = l + (h-l)/2;
+            bool isEven = false;
+            
+            if((h-m)%2 == 0){
+                isEven = true; // there are even number of elements present in right side of nums[m]
             }
-            if (nums[ind] != nums[ind+1]){
-                j = mid-1;
-                ans = nums[ind];
+            else if((h-m)%2 == 1){
+                isEven = false; // there are odd number of elements present in right side of nums[m]
             }
-            else{
-                i = mid+1;
+            
+            if(nums[m] == nums[m+1]){
+                if(isEven){
+                    l = m + 2;
+                }else{
+                    h = m - 1;
+                }
+            }else{
+                if(isEven){
+                    h = m;
+                }else{
+                    l = m + 1;
+                }
             }
         }
-        return ans;
+        return nums[l];
     }
 };
