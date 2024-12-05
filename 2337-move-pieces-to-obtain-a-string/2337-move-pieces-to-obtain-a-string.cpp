caf232d0e056bf04f75be9f4306a595885e9a01e
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-05 11:37:12 +0530

    Time: 9 ms (64.37%), Space: 21.9 MB (33.33%) - LeetHub

--- 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.cpp
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.cpp
@@ -2,34 +2,39 @@ class Solution {
 public:
     bool canChange(string start, string target) {
         int len = start.size();
-        //char c1 = '_', c2 = '_';
         int i = 0, j = 0;
-        while (i < len && j < len){
-            while (i < len && start[i] == '_'){
+        while (i < len && j < len) {
+            while (i < len && start[i] == '_') {
                 i++;
             }
-            while (j < len && target[j] == '_'){
+            while (j < len && target[j] == '_') {
                 j++;
             }
-            if (i < len && j >= len) return false;
-            if (j < len && i >= len) return false;
-            if (i < len && j < len){
-                if (start[i] != target[j]) return false;
+            if (i < len && j >= len)
+                return false;
+            if (j < len && i >= len)
+                return false;
+            if (i < len && j < len) {
+                if (start[i] != target[j])
+                    return false;
                 else {
-                    if (start[i] == 'L' && j > i) return false;
-                    else if (start[i] == 'R' && j < i) return false;
+                    if (start[i] == 'L' && j > i)
+                        return false;
+                    else if (start[i] == 'R' && j < i)
+                        return false;
                 }
             }
-            i++; j++;
+            i++;
+            j++;
         }
-        cout << i<< "done"<< j;
-        while (i < len){
-            //cout<< i << " ";
-            if (start[i] != '_') return false;
+        while (i < len) {
+            if (start[i] != '_')
+                return false;
             i++;
         }
-        while (j < len){
-            if (target[j] != '_') return false;
+        while (j < len) {
+            if (target[j] != '_')
+                return false;
             j++;
         }
         return true;
