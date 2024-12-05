88895876971f014ec5caa9293da56f9a10315f33
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-05 11:33:13 +0530

    Time: 11 ms (55.75%), Space: 21.8 MB (37.84%) - LeetHub

--- 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.cpp
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.cpp
@@ -3,30 +3,38 @@ public:
     bool canChange(string start, string target) {
         int len = start.size();
         int i = 0, j = 0;
-        while (i < len && j < len){
-            while (i < len && start[i] == '_'){
+        while (i < len && j < len) {
+            while (i < len && start[i] == '_') {
                 i++;
             }
-            while (j < len && target[j] == '_'){
+            while (j < len && target[j] == '_') {
                 j++;
             }
-            if (i < len && j >= len) return false;
-            if (j < len && i >= len) return false;
-            if (i < len && j < len){
-                if (start[i] != target[j]) return false;
+            if (i < len && j >= len)
+                return false;
+            if (j < len && i >= len)
+                return false;
+            if (i < len && j < len) {
+                if (start[i] != target[j])
+                    return false;
                 else {
-                    if (start[i] == 'L' && j > i) return false;
-                    else if (start[i] == 'R' && j < i) return false;
+                    if (start[i] == 'L' && j > i)
+                        return false;
+                    else if (start[i] == 'R' && j < i)
+                        return false;
                 }
             }
-            i++; j++;
+            i++;
+            j++;
         }
-        while (i < len){
-            if (start[i] != '_') return false;
+        while (i < len) {
+            if (start[i] != '_')
+                return false;
             i++;
         }
-        while (j < len){
-            if (target[j] != '_') return false;
+        while (j < len) {
+            if (target[j] != '_')
+                return false;
             j++;
         }
         return true;
