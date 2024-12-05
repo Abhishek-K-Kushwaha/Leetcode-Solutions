c93d3c509366b4b3c926d225471db2d1574c39fe
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-05 11:44:26 +0530

    Time: 15 ms (43.1%), Space: 22 MB (33.33%) - LeetHub

--- 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.cpp
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.cpp
@@ -3,17 +3,17 @@ public:
     bool canChange(string start, string target) {
         int len = start.size();
         int i = 0, j = 0;
-        while (i < len && j < len) {
+        while (i < len || j < len) {
             while (i < len && start[i] == '_') {
                 i++;
             }
             while (j < len && target[j] == '_') {
                 j++;
             }
-            if (i < len && j >= len)
-                return false;
-            if (j < len && i >= len)
-                return false;
+            // if (i < len && j >= len)
+            //     return false;
+            // if (j < len && i >= len)
+            //     return false;
             //if (i < len && j < len) {
                 if (start[i] != target[j])
                     return false;
@@ -27,16 +27,16 @@ public:
             i++;
             j++;
         }
-        while (i < len) {
-            if (start[i] != '_')
-                return false;
-            i++;
-        }
-        while (j < len) {
-            if (target[j] != '_')
-                return false;
-            j++;
-        }
+        // while (i < len) {
+        //     if (start[i] != '_')
+        //         return false;
+        //     i++;
+        // }
+        // while (j < len) {
+        //     if (target[j] != '_')
+        //         return false;
+        //     j++;
+        // }
         return true;
     }
 };
