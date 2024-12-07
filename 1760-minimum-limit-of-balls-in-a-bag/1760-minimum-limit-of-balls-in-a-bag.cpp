a7333d96fad2a249fb56bdfd5a19a414e65d4b2d
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-07 18:33:52 +0530

    Time: 39 ms (41.78%), Space: 59.7 MB (28.46%) - LeetHub

--- 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
@@ -1,11 +1,14 @@
 class Solution {
 private:
-    bool f(int m, vector<int>& nums, int maxop){
+    bool f(int m, vector<int>& nums, int maxop) {
         int cnt = 0;
-        for (int i:nums){
-            if (i%m) cnt = cnt+(i/m)+1;
-            else cnt += (i/m);
-            if (cnt > maxop) return false;
+        for (int i : nums) {
+            if (i % m)
+                cnt = cnt + (i / m) + 1;
+            else
+                cnt += (i / m);
+            if (cnt > maxop)
+                return false;
         }
         return true;
     }
@@ -15,10 +18,12 @@ public:
         int n = nums.size();
         int l = 1;
         int r = *max_element(nums.begin(), nums.end());
-        while (l < r){
-            int m = (l+r)/2;
-            if (f(m, nums, n+maxOperations)) r = m;
-            else l = m + 1;
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (f(m, nums, n + maxOperations))
+                r = m;
+            else
+                l = m + 1;
         }
         return r;
     }
