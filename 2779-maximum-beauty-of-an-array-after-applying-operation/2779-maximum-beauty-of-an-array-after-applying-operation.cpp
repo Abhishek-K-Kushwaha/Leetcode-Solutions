4271b4fa6b49fbc585ae02de9075ce369a2f39f9
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-11 10:03:25 +0530

    Time: 55 ms (54.68%), Space: 105.1 MB (67.7%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -4,16 +4,16 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
         int l = 1, r = n, mid;
-        while (l < r){
-            mid = (l+r+1)/2;
-            //cout<< mid <<endl;
-            for (int i = mid-1; i < n; i++){
-                if (nums[i] - nums[i-mid+1] <= 2*k){
+        while (l < r) {
+            mid = (l + r + 1) / 2;
+            for (int i = mid - 1; i < n; i++) {
+                if (nums[i] - nums[i - mid + 1] <= 2 * k) {
                     l = mid;
                     break;
                 }
             }
-            if (l != mid) r = mid-1;
+            if (l != mid)
+                r = mid - 1;
         }
         return l;
     }
