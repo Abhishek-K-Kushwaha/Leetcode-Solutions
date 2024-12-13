6421a1141d8f9e8779db5e6d36451ac09e856c3a
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-13 18:44:25 +0530

    Time: 961 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,6 +1,9 @@
 # Write your MySQL query statement below
 
 select p.firstName, p.lastName, a.city, a.state
-from Person as p left join 
-Address as a
-on p.personId = a.personId;
+from Person as p, Address as a
+where p.personId = a.personId
+union
+select p.firstName, p.lastName, null, null 
+from Person as p
+where p.personId not in (select personId from Address);
