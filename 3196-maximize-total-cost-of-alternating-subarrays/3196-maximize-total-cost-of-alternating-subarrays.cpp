54cdda1bf9292b7a18b4940947daa912c3105540
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-13 19:34:54 +0530

    Time: 88 ms (34.58%), Space: 123.7 MB (20.18%) - LeetHub

--- 3196-maximize-total-cost-of-alternating-subarrays/3196-maximize-total-cost-of-alternating-subarrays.cpp
+++ 3196-maximize-total-cost-of-alternating-subarrays/3196-maximize-total-cost-of-alternating-subarrays.cpp
@@ -5,11 +5,11 @@ public:
         vector<vector<long long>> dp(n, vector<long long> (3));
         dp[0][0] = nums[0], dp[0][1] = nums[0], dp[0][2] = nums[0];
         for (int i = 1; i < n; i++){
-            dp[i][0] = max(dp[i-1][0], max(dp[i-1][1], dp[i-1][2])) + nums[i];
-            dp[i][1] = dp[i-1][2]+nums[i];
-            dp[i][2]= max(dp[i-1][0], dp[i-1][1])-nums[i];
+            dp[i][0] = max(dp[i-1][0], max(dp[i-1][2], dp[i-1][2])) + nums[i];
+            //dp[i][1] = dp[i-1][2]+nums[i];
+            dp[i][2]= max(dp[i-1][0], dp[i-1][0])-nums[i];
             //cout << dp[i][0] << " " <<dp[i][1] << " "<< dp[i][2]<< endl;
         } 
-        return max(dp[n-1][0], max(dp[n-1][1], dp[n-1][2]));
+        return max(dp[n-1][0], max(dp[n-1][2], dp[n-1][2]));
     }
 };
