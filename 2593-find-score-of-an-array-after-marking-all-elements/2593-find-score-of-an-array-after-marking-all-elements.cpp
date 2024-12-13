6c6a165c61b461963b8242e8e4576e6380ab381e
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-13 18:10:26 +0530

    Time: 97 ms (77.26%), Space: 96.2 MB (98.04%) - LeetHub

--- 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
     long long findScore(vector<int>& nums) {
-        int n = nums.size();
-        set<pair<int,int>> st;
-        for (int i = 0; i< n; i++){
-            st.insert({nums[i], i});
-        }
-        vector<bool> marked(n, false);
-        int cnt = 0;
-        long long ans = 0;
-        while (cnt < n){
-            auto [x,y] = *st.begin();
-            ans += x;
-            //cout << ans << endl;
-            st.erase({x,y});
-            marked[y] = 1;
-            cnt++;
-            if (y > 0 && !marked[y-1]){
-                st.erase({nums[y-1], y-1});
-                cnt++;
-                marked[y-1] = 1;
-            }
-            if (y < n-1 && !marked[y+1]){
-                st.erase({nums[y+1], y+1});
-                cnt++;
-                marked[y+1] = 1;
-            }
+        int n = nums.size();  // Get the size of the array nums
+        vector<int> ord(n);  // Array to store the indices of nums
+        iota(ord.begin(), ord.end(), 0);  // Initialize ord with 0, 1, 2, ..., n-1
 
-        }
-        return ans;
+        // Sort the indices based on the values in nums
+        sort(ord.begin(), ord.end(), [&](int a, int b) {
+            if (nums[a] != nums[b])
+                return nums[a] < nums[b];  // Sort by value of nums
+            return a < b;  // If values are the same, sort by index
+        });
 
+        vector<bool> m(n, false);  // Array to mark which elements are already processed
+        long long score = 0;  // Initialize score to 0
+
+        // Iterate over the sorted indices to calculate the score
+        for (int x : ord) {
+            if (m[x])  // If the element is already marked, skip it
+                continue;
+
+            score += nums[x];  // Add the value of nums[x] to the score
+            m[x] = true;  // Mark the current element as processed
+
+            // Mark the previous element as processed, if it exists
+            if (x > 0)
+                m[x - 1] = true;
+
+            // Mark the next element as processed, if it exists
+            if (x + 1 < n)
+                m[x + 1] = true;
+        }
+        return score;  // Return the total score
     }
 };
