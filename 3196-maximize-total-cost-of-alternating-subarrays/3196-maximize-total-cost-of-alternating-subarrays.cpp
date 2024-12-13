8d129c9313e783cef64cb367f0c26f630f4d1376
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-13 19:29:50 +0530

    Time: 104 ms (25.53%), Space: 123.4 MB (20.64%) - LeetHub

---
+++ 3196-maximize-total-cost-of-alternating-subarrays/3196-maximize-total-cost-of-alternating-subarrays.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long maximumTotalCost(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<long long>> dp(n, vector<long long> (3));
+        dp[0][0] = nums[0], dp[0][1] = nums[0], dp[0][2] = nums[0];
+        for (int i = 1; i < n; i++){
+            dp[i][0] = max(dp[i-1][0], max(dp[i-1][1], dp[i-1][2])) + nums[i];
+            dp[i][1] = dp[i-1][2]+nums[i];
+            dp[i][2]= max(dp[i-1][0], dp[i-1][1])-nums[i];
+            //cout << dp[i][0] << " " <<dp[i][1] << " "<< dp[i][2]<< endl;
+        } 
+        return max(dp[n-1][0], max(dp[n-1][1], dp[n-1][2]));
+    }
+};
