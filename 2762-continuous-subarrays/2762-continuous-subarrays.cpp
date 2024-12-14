65b13d3fa0b10b89f7b03baf1d41fbc83c152c4c
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-14 20:03:11 +0530

    Time: 89 ms (86.23%), Space: 121.6 MB (47.34%) - LeetHub

--- 2762-continuous-subarrays/2762-continuous-subarrays.cpp
+++ 2762-continuous-subarrays/2762-continuous-subarrays.cpp
@@ -1,28 +1,41 @@
+#include <vector>
+#include <deque>
+
+using namespace std;
+
 class Solution {
 public:
     long long continuousSubarrays(vector<int>& nums) {
-        long long ans = nums.size(), n = ans;
-        int l = 0, r = 1, len;
-        multiset<int> st;
-        st.insert(nums[0]);
-        while (r < n) {
-            while (r < n &&
-                   (st.empty() || abs(nums[r] - *st.begin()) <= 2 &&
-                                  abs(nums[r] - *st.rbegin()) <= 2)) {
-                st.insert(nums[r++]);
+        long long ans = 0;
+        int n = nums.size();
+        deque<int> minDeque, maxDeque; // To track min and max in the window
+        int l = 0;
+
+        for (int r = 0; r < n; ++r) {
+            // Maintain minDeque for the minimum value
+            while (!minDeque.empty() && nums[minDeque.back()] > nums[r]) {
+                minDeque.pop_back();
             }
-            len = r - l;
-            ans += ((long long)len * (len - 1)) / 2;
-            while (l < r && r < n &&
-                   ((abs(nums[r] - *st.begin()) > 2) ||
-                    (abs(nums[r] - *st.rbegin()) > 2))) {
-                auto it = st.find(nums[l++]);
-                st.erase(it);
+            minDeque.push_back(r);
+
+            // Maintain maxDeque for the maximum value
+            while (!maxDeque.empty() && nums[maxDeque.back()] < nums[r]) {
+                maxDeque.pop_back();
             }
-            len = r - l;
-            if (r < n)
-                ans -= ((long long)len * (len - 1)) / 2;
+            maxDeque.push_back(r);
+
+            // Shrink the window if the difference exceeds 2
+            while (!minDeque.empty() && !maxDeque.empty() &&
+                   nums[maxDeque.front()] - nums[minDeque.front()] > 2) {
+                if (minDeque.front() == l) minDeque.pop_front();
+                if (maxDeque.front() == l) maxDeque.pop_front();
+                ++l;
+            }
+
+            // Add the number of valid subarrays ending at r
+            ans += (r - l + 1);
         }
+
         return ans;
     }
 };
