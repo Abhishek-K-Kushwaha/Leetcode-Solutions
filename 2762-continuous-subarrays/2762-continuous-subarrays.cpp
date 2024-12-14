c59b31d2630925c6088d6c7b87d486b9bbcc2272
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-14 20:02:47 +0530

    Time: 229 ms (26.65%), Space: 156 MB (7%) - LeetHub

--- 2762-continuous-subarrays/2762-continuous-subarrays.cpp
+++ 2762-continuous-subarrays/2762-continuous-subarrays.cpp
@@ -3,32 +3,25 @@ public:
     long long continuousSubarrays(vector<int>& nums) {
         long long ans = nums.size(), n = ans;
         int l = 0, r = 1, len;
-        multiset<int> st; 
+        multiset<int> st;
         st.insert(nums[0]);
-        while (r < n){
-            //cout << l << " " << r << endl;
-            while (r < n && (st.empty() || abs(nums[r]-*st.begin())<=2 && abs(nums[r]-*st.rbegin())<=2)){
+        while (r < n) {
+            while (r < n &&
+                   (st.empty() || abs(nums[r] - *st.begin()) <= 2 &&
+                                  abs(nums[r] - *st.rbegin()) <= 2)) {
                 st.insert(nums[r++]);
-                //r++;
-                // cout << "r= " <<r << endl;
             }
             len = r - l;
-            ans += ((long long)len*(len-1))/2;
-            // cout << ans << " ans" << endl;
-            while (l<r && r<n && ((abs(nums[r]-*st.begin())>2) || (abs(nums[r]-*st.rbegin())>2))){
+            ans += ((long long)len * (len - 1)) / 2;
+            while (l < r && r < n &&
+                   ((abs(nums[r] - *st.begin()) > 2) ||
+                    (abs(nums[r] - *st.rbegin()) > 2))) {
                 auto it = st.find(nums[l++]);
-                //cout << "To be del " << *it<< endl;
                 st.erase(it);
-                // for (auto i: st){
-                //     cout << i << " ";
-                // }
-                //l++;
-                //cout << endl << "l = " << l << endl;
             }
-            // if (r<n) st.insert(nums[r]);
             len = r - l;
-            if (r<n)ans -= ((long long)len*(len-1))/2;
-            // r++;
+            if (r < n)
+                ans -= ((long long)len * (len - 1)) / 2;
         }
         return ans;
     }
