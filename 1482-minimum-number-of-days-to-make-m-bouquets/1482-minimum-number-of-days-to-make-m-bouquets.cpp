71dc23449aa73f12d89d9f6ddc9783659566f2c4
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-16 16:02:34 +0530

    Time: 31 ms (21.58%), Space: 70.4 MB (37.85%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     bool f(int d, int m, int k, int n, vector<int>& bloomDay){
         int cnt = 0, str = 0;
-        //cout << d << endl;
         for (int i = 0; i < n; i++){
             if (bloomDay[i] <= d){
                 str++;
                 if (str == k){
                     str = 0;
                     cnt++;
-                    //cout << "cnt= "<< cnt<< endl;
                     if (cnt == m) return true;
                 }
             }
@@ -22,7 +20,6 @@ public:
         if ((long long)m*k > n) return -1;
         int l = 1, r = 1e9, mid;
         while (l < r){
-            //cout << l << " "<< r<<endl;
             mid= (l+r)/2;
             if (f(mid, m, k, n, bloomDay)) r = mid;
             else l = mid + 1;
