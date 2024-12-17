e6a6b2ae7e37c9e1be41d0d05e51e3303838c1db
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-17 20:16:33 +0530

    Time: 9 ms (94.68%), Space: 27.7 MB (52.21%) - LeetHub

---
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string repeatLimitedString(string s, int lim) {
+        vector<int> freq(26, 0);
+        for (auto it:s){
+            freq[it-'a']++;
+        }
+        // for (auto i:freq){
+        //     cout << i << " ";
+        // }
+        //cout << endl;
+        int r = 25, l = r - 1, cnt = 0;
+        string ans;
+        while (r >= 0){
+            while (r >= 0 && !freq[r]){
+                r--;
+            }
+            //cout << r << endl;
+            if (r < 0) break;
+            cnt = 0;
+            while (freq[r] > 0 && cnt < lim){
+                ans.push_back('a'+r);
+                cnt++;
+                freq[r]--;
+            }
+            if (freq[r] == 0){ 
+                r--;
+                continue;
+            }
+            l = r-1;
+            while (l >= 0 && !freq[l]){
+                l--;
+            }
+            if (l >= 0){ 
+                ans.push_back('a'+l);
+                freq[l]--;
+            }
+            else break;
+            //cout << ans << endl;
+        }
+        return ans;
+    }
+};
