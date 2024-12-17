c3d458cb23cb14215970b60e6643dc157375c221
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-17 20:23:36 +0530

    Time: 8 ms (95.44%), Space: 27.7 MB (59.07%) - LeetHub

--- 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.cpp
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.cpp
@@ -2,35 +2,36 @@ class Solution {
 public:
     string repeatLimitedString(string s, int lim) {
         vector<int> freq(26, 0);
-        for (auto it:s){
-            freq[it-'a']++;
+        for (auto it : s) {
+            freq[it - 'a']++;
         }
         int r = 25, l = r - 1, cnt = 0;
         string ans;
-        while (r >= 0){
-            while (r >= 0 && !freq[r]){
+        while (r >= 0) {
+            while (r >= 0 && !freq[r]) {
                 r--;
             }
-            if (r < 0) break;
+            if (r < 0)
+                break;
             cnt = 0;
-            while (freq[r] > 0 && cnt < lim){
-                ans.push_back('a'+r);
+            while (freq[r] > 0 && cnt < lim) {
+                ans.push_back('a' + r);
                 cnt++;
                 freq[r]--;
             }
-            if (freq[r] == 0){ 
+            if (freq[r] == 0) {
                 r--;
                 continue;
             }
-            l = r-1;
-            while (l >= 0 && !freq[l]){
+            l = r - 1;
+            while (l >= 0 && !freq[l]) {
                 l--;
             }
-            if (l >= 0){ 
-                ans.push_back('a'+l);
+            if (l >= 0) {
+                ans.push_back('a' + l);
                 freq[l]--;
-            }
-            else break;
+            } else
+                break;
         }
         return ans;
     }
