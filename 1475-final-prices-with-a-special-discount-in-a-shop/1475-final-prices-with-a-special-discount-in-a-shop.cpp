f913d21f3acb79029360e84a140ee2219cf2d7ec
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-18 20:12:19 +0530

    Time: 0 ms (100%), Space: 14.1 MB (24.39%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -2,12 +2,16 @@ class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
         int n = prices.size();
+        stack<int> stk;
         int j;
-        for (int i = 0; i < n; i++){
-            j = i+1;
-            while (j < n && prices[j] > prices[i]) j++;
-            if (j >= n) continue;
-            else prices[i] -= prices[j];
+        for (int i = 0; i < n; i++) {
+            while (!stk. empty() && prices[i] <= prices[stk.top()]){
+                j = stk.top();
+                prices[j] -= prices[i];
+                //cout << prices[j] << endl;
+                stk.pop();
+            }
+            stk.push(i);
         }
         return prices;
     }
