257966bbff3dd62fce435966c463814bf9817008
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-20 15:36:46 +0530

    Time: 8 ms (20.68%), Space: 82 MB (41.35%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cpp
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cpp
@@ -12,14 +12,11 @@
 class Solution {
 public:
     TreeNode* reverseOddLevels(TreeNode* root) {
-        //vector<int> val;
         if (root->left == nullptr) return root;
-        queue<TreeNode*> q;
-        queue<TreeNode*> revq;
+        queue<TreeNode*> q; queue<TreeNode*> revq;
         q.push(root->left); revq.push(root->right);
         int lvl = 1, cnt = 0, num;
-        TreeNode* temp;
-        TreeNode* revtemp;
+        TreeNode* temp; TreeNode* revtemp;
         while (q.front() != nullptr){
             temp = q.front();
             revtemp = revq.front();
@@ -28,11 +25,7 @@ public:
             q.push(temp->right);
             revq.push(revtemp->right);
             revq.push(revtemp->left);
-            //cout<< lvl << endl;
-            
             if (lvl % 2){
-                //val.push_back(temp->val);
-                //cout << temp->val <<" "<< revtemp->val<< endl;
                 num = temp->val;
                 temp->val = revtemp->val;
                 revtemp->val = num;
