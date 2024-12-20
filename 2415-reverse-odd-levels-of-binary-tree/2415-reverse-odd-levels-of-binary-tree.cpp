90ab9320240d1088a56b890d387df69b46fc2211
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-20 15:36:58 +0530

    Time: 6 ms (28.22%), Space: 81.9 MB (44.08%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cpp
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cpp
@@ -6,36 +6,42 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* reverseOddLevels(TreeNode* root) {
-        if (root->left == nullptr) return root;
-        queue<TreeNode*> q; queue<TreeNode*> revq;
-        q.push(root->left); revq.push(root->right);
+        if (root->left == nullptr)
+            return root;
+        queue<TreeNode*> q;
+        queue<TreeNode*> revq;
+        q.push(root->left);
+        revq.push(root->right);
         int lvl = 1, cnt = 0, num;
-        TreeNode* temp; TreeNode* revtemp;
-        while (q.front() != nullptr){
+        TreeNode* temp;
+        TreeNode* revtemp;
+        while (q.front() != nullptr) {
             temp = q.front();
             revtemp = revq.front();
-            q.pop(); revq.pop();
+            q.pop();
+            revq.pop();
             q.push(temp->left);
             q.push(temp->right);
             revq.push(revtemp->right);
             revq.push(revtemp->left);
-            if (lvl % 2){
+            if (lvl % 2) {
                 num = temp->val;
                 temp->val = revtemp->val;
                 revtemp->val = num;
             }
             cnt += 4;
-            if (cnt == pow(2,lvl+1)){
+            if (cnt == pow(2, lvl + 1)) {
                 lvl++;
                 cnt = 0;
             }
-        } 
+        }
         return root;
     }
 };
