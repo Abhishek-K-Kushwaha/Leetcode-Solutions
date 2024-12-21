92be49e13e4194b9ce6e1feff9bc067364161e1f
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-21 14:12:47 +0530

    Time: 134 ms (90%), Space: 171.8 MB (90%) - LeetHub

--- 2858-minimum-edge-reversals-so-every-node-is-reachable/2858-minimum-edge-reversals-so-every-node-is-reachable.cpp
+++ 2858-minimum-edge-reversals-so-every-node-is-reachable/2858-minimum-edge-reversals-so-every-node-is-reachable.cpp
@@ -1,46 +1,45 @@
 class Solution {
 public:
-    int to, away, ans;
-    void dfs2(int node, vector<vector<pair<int,bool>>>& adj, vector<int>& res){
-        for (auto [nxt,sign]:adj[node]){
-            if (res[nxt] == -1){
-                // if (!sign) to++;
-                // else away++;
-                to += (!sign);
-                away += sign;
-                res[nxt] = res[0]+away-to;
-                dfs2(nxt, adj, res);
-                // if (!sign) to--;
-                // else away--;
-                to -= (!sign);
-                away -= sign;
-            }
-        }
-    }
-    void dfs(int node, vector<vector<pair<int,bool>>>& adj, int parent){
-        for (auto [nxt,sign]:adj[node]){
-            if (nxt != parent){
-                if (!sign) {
-                    to++;
-                }
-                dfs(nxt, adj, node);
-            }
-        }
-    }
     vector<int> minEdgeReversals(int n, vector<vector<int>>& edges) {
-        vector<vector<pair<int,bool>>> adj(n);
-        int a, b;
-        for (auto it: edges){
-            a = it[0], b = it[1];
-            adj[a].push_back({b,1});
-            adj[b].push_back({a,0});
+        // Build adjacency list with edge directions
+        vector<vector<pair<int,int>>> adj(n);
+        for (const auto& edge : edges) {
+            adj[edge[0]].emplace_back(edge[1], 0);  // 0 for forward edge
+            adj[edge[1]].emplace_back(edge[0], 1);  // 1 for reverse edge
         }
-        vector<int> res(n, -1);
-        to =  0;
-        dfs(0, adj, -1);
-        res[0] = to;
-        to = 0, away = 0;
-        dfs2(0, adj, res);
-        return res;
+        
+        vector<int> result(n);
+        
+        // First DFS from node 0 to compute base costs
+        int total_reversals = dfs1(0, -1, adj);
+        result[0] = total_reversals;
+        
+        // Second DFS to compute costs for all other nodes
+        dfs2(0, -1, total_reversals, adj, result);
+        
+        return result;
+    }
+    
+private:
+    int dfs1(int node, int parent, const vector<vector<pair<int,int>>>& adj) {
+        int reversals = 0;
+        for (const auto& [next, isReverse] : adj[node]) {
+            if (next != parent) {
+                reversals += isReverse + dfs1(next, node, adj);
+            }
+        }
+        return reversals;
+    }
+    
+    void dfs2(int node, int parent, int parentCost, const vector<vector<pair<int,int>>>& adj, vector<int>& result) {
+        for (const auto& [next, isReverse] : adj[node]) {
+            if (next != parent) {
+                // If edge was forward (0), it becomes reverse (1) when changing root
+                // If edge was reverse (1), it becomes forward (0) when changing root
+                int newCost = parentCost + (isReverse == 0 ? 1 : -1);
+                result[next] = newCost;
+                dfs2(next, node, newCost, adj, result);
+            }
+        }
     }
 };
