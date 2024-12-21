3dec4d3daf31adb2e06ae3fff0fd165f0864d584
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-21 14:17:31 +0530

    Time: 80 ms (94.17%), Space: 104.9 MB (86.09%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int dfs(vector<vector<int>> &al, int parent, int from) {
         auto change = 0;
-        //visited[from] = true;
         for (auto to : al[from])
             if (abs(to) != parent)
                 change += dfs(al, abs(from), abs(to)) + (to > 0);
@@ -11,8 +10,8 @@ public:
     int minReorder(int n, vector<vector<int>>& connections) {
         vector<vector<int>> al(n);
         for (auto &c : connections) {
-            al[c[0]].push_back(c[1]);
-            al[c[1]].push_back(-c[0]);
+            al[c[0]].emplace_back(c[1]);
+            al[c[1]].emplace_back(-c[0]);
         }
         return dfs(al, -1, 0);
     }
