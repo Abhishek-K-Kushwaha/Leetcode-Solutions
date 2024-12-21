ebb5a9968ff85e564c27045294bf059d41ab00fc
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-21 11:55:52 +0530

    Time: 139 ms (59.07%), Space: 116.3 MB (50.18%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    void dfs(int node, vector<vector<pair<int,bool>>>& adj, int& ans, int parent){
-        for (auto& [nxt,sign]:adj[node]){
-            //int nxt = it.first;
-            //bool sign = it.second;
-            if (nxt != parent){
+    void dfs(int node, vector<vector<pair<int, bool>>>& adj, int& ans,
+             int parent) {
+        for (auto& [nxt, sign] : adj[node]) {
+            if (nxt != parent) {
                 ans += sign;
                 dfs(nxt, adj, ans, node);
             }
         }
     }
     int minReorder(int n, vector<vector<int>>& edges) {
-        vector<vector<pair<int,bool>>> adj(n);
+        vector<vector<pair<int, bool>>> adj(n);
         int a, b;
-        for (auto it: edges){
+        for (auto it : edges) {
             a = it[0], b = it[1];
-            adj[a].push_back({b,1});
-            adj[b].push_back({a,0});
+            adj[a].push_back({b, 1});
+            adj[b].push_back({a, 0});
         }
         int ans = 0;
         dfs(0, adj, ans, -1);
