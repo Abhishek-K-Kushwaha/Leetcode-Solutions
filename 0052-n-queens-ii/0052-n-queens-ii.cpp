432fef2bcafcc8052205ebcac2ad12dcb4221def
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-22 19:34:33 +0530

    Time: 0 ms (100%), Space: 7.9 MB (66.51%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    //vector<int> r(9,0);
-    
     int ans = 0;
-    void put(int r, int n, vector<bool>& c, vector<bool>& d, vector<bool>& invd){
-        //cout << r<< endl;
-        if (r == n){
+    void put(int r, int n, vector<bool>& c, vector<bool>& d,
+             vector<bool>& invd) {
+        if (r == n) {
             ans++;
             return;
         }
-        //int x, y;
-        for (int i = 0; i < n; i++){
-            if (!c[i] && !d[r+i] && !invd[r-i+8]){
-                //cout<<"yes\n";
-                c[i] = 1, d[r+i] = 1 , invd[r-i+8] = 1;
-                put(r+1, n, c, d, invd);
-                c[i] = 0, d[r+i] = 0 , invd[r-i+8] = 0;
+        for (int i = 0; i < n; i++) {
+            if (!c[i] && !d[r + i] && !invd[r - i + 8]) {
+                c[i] = 1, d[r + i] = 1, invd[r - i + 8] = 1;
+                put(r + 1, n, c, d, invd);
+                c[i] = 0, d[r + i] = 0, invd[r - i + 8] = 0;
             }
         }
     }
     int totalNQueens(int n) {
-        vector<bool> c(9,0);
-        vector<bool> d(17,0); //i = sum-1 /-shaped
-        vector<bool> invd(17,0); //i = diff+7 \-shaped 
+        vector<bool> c(9, 0);
+        vector<bool> d(17, 0);    // i = sum-1 /-shaped
+        vector<bool> invd(17, 0); // i = diff+7 \-shaped
         put(0, n, c, d, invd);
         return ans;
     }
