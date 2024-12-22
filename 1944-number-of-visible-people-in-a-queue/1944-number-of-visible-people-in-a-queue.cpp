2c40867f172ad6529d0b7c71a13baa7323431b6c
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-22 18:10:15 +0530

    Time: 20 ms (41.52%), Space: 88.7 MB (16.36%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
@@ -3,15 +3,15 @@ public:
     vector<int> canSeePersonsCount(vector<int>& heights) {
         int n = heights.size();
         vector<int> ans(n,0);
-        vector<int> stk;
-        stk.emplace_back(heights[n-1]);
+        stack<int> stk;
+        stk.push(heights[n-1]);
         for (int i = n-2; i >= 0; i--){
-            while (!stk.empty() && stk.back() < heights[i]){
+            while (!stk.empty() && stk.top() < heights[i]){
                 ans[i]++;
-                stk.pop_back();
+                stk.pop();
             }
             if (!stk.empty())ans[i]++;
-            stk.emplace_back(heights[i]);
+            stk.push(heights[i]);
         }
         return ans;
     }
