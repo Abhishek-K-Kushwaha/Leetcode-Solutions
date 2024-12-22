49b0779a681f462875a8639d5a5254ff53b3ac35
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-22 16:58:01 +0530

    Time: 56 ms (93.63%), Space: 262.8 MB (59.24%) - LeetHub

--- 2940-find-building-where-alice-and-bob-can-meet/2940-find-building-where-alice-and-bob-can-meet.cpp
+++ 2940-find-building-where-alice-and-bob-can-meet/2940-find-building-where-alice-and-bob-can-meet.cpp
@@ -1,46 +1,48 @@
 class Solution {
 public:
-    int f(int h, vector<pair<int,int>>& stk){
+    int f(int h, vector<pair<int, int>>& stk) {
         int l = 0;
-        int r = stk.size()- 1;
+        int r = stk.size() - 1;
         int ans = -1;
-        while (l <= r){
-            int mid = (l+r)/2;
-            if (stk[mid].first > h){
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (stk[mid].first > h) {
                 ans = max(ans, mid);
                 l = mid + 1;
-            }
-            else r = mid - 1;
+            } else
+                r = mid - 1;
         }
         return ans;
     }
-    vector<int> leftmostBuildingQueries(vector<int>& heights, vector<vector<int>>& queries) {
+    vector<int> leftmostBuildingQueries(vector<int>& heights,
+                                        vector<vector<int>>& queries) {
         int n = heights.size();
-        vector<vector<pair<int,int>>> q(n);
+        vector<vector<pair<int, int>>> q(n);
         vector<int> ans(queries.size(), -1);
         int i = 0;
-        for (auto& it:queries){
-            if (it[0] > it[1]){
+        for (auto& it : queries) {
+            if (it[0] > it[1]) {
                 swap(it[0], it[1]);
             }
-            if (it[0] == it[1] || heights[it[0]] < heights[it[1]]){
+            if (it[0] == it[1] || heights[it[0]] < heights[it[1]]) {
                 ans[i] = it[1];
-            }
-            else{
+            } else {
                 q[it[1]].push_back({heights[it[0]], i});
             }
             i++;
         }
-        
-        vector<pair<int,int>> stk; //height, index
-        for (int i = n-1; i >= 0; i--){
-            for (auto& it:q[i]){
+
+        vector<pair<int, int>> stk; // height, index
+        for (int i = n - 1; i >= 0; i--) {
+            for (auto& it : q[i]) {
                 int h = it.first;
                 int ind = it.second;
                 int pos = f(h, stk);
-                if (pos >= 0) ans[ind] = stk[pos].second;
+                if (pos >= 0)
+                    ans[ind] = stk[pos].second;
             }
-            while (!stk.empty() && stk.back().first < heights[i]) stk.pop_back();
+            while (!stk.empty() && stk.back().first < heights[i])
+                stk.pop_back();
             stk.push_back({heights[i], i});
         }
         return ans;
