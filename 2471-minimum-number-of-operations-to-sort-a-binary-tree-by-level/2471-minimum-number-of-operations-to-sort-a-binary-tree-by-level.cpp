d7ccdfde812a7046c8b3a5edc20747a0fd8f21c0
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-23 14:09:21 +0530

    Time: 51 ms (92.96%), Space: 174.6 MB (84.16%) - LeetHub

--- 2471-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2471-minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
+++ 2471-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2471-minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
@@ -6,7 +6,8 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
@@ -15,23 +16,25 @@ public:
         queue<TreeNode*> q;
         q.push(root);
         int ans = 0;
-        while (!q.empty()){
+        while (!q.empty()) {
             int n = q.size();
-            vector<pair<int,int>> arr(n);
+            vector<pair<int, int>> arr(n);
             vector<int> vis(n, 0);
-            for (int i = 0; i < n; i++){
+            for (int i = 0; i < n; i++) {
                 TreeNode* node = q.front();
                 q.pop();
                 arr[i] = {node->val, i};
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
+                if (node->left)
+                    q.push(node->left);
+                if (node->right)
+                    q.push(node->right);
             }
             sort(arr.begin(), arr.end());
-            for (int i = 0; i < n; i++){
-                if (!vis[i]){
+            for (int i = 0; i < n; i++) {
+                if (!vis[i]) {
                     vis[i] = 1;
                     int ind = i;
-                    while (!vis[arr[ind].second]){
+                    while (!vis[arr[ind].second]) {
                         ans++;
                         vis[arr[ind].second] = 1;
                         ind = arr[ind].second;
