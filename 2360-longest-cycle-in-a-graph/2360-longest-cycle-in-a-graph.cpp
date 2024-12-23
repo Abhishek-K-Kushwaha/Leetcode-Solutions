86761ca836f782b0d52514dbae5734e2b78a0081
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-23 16:09:10 +0530

    Time: 251 ms (40.54%), Space: 157.4 MB (27.9%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -2,27 +2,29 @@ class Solution {
 public:
     int ans = 0;
     int d;
-    void dfs(int node, vector<int>& edges, unordered_map<int,int>& pathvis, 
-    vector<int>& vis){
-        vis[node] = 1;
+    void dfs(int node, vector<int>& edges, unordered_map<int,int>& pathvis){
         pathvis[node] = d;
         int nxt = edges[node];
-        if (pathvis.find(nxt) != pathvis.end()){
+        if (pathvis[nxt] > 1){
             ans = max(ans, d - pathvis[nxt] + 1);
+            pathvis[node]=1;
             return;
         }
-        if (nxt == -1 || vis[nxt]) return;
+        if (nxt == -1 || pathvis[nxt]){ 
+            pathvis[node]=1;
+            return;
+        }
         d++;
-        dfs(nxt, edges, pathvis, vis);
+        dfs(nxt, edges, pathvis);
+        pathvis[node]=1;
     }
     int longestCycle(vector<int>& edges) {
         int n = edges.size();
-        vector<int> vis(n,0);
+        unordered_map<int,int> pathvis;
         for (int i = 0; i < n; i++){
-            if (!vis[i]){
-                unordered_map<int,int> pathvis;
-                d = 1;
-                dfs(i, edges, pathvis, vis);
+            if (!pathvis[i]){
+                d = 2;
+                dfs(i, edges, pathvis);
             }
         }
         if (ans) return ans;
