eb3e8ea6d71f501b7ef384da53c8003162964eb9
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-23 14:44:39 +0530

    Time: 331 ms (34.28%), Space: 211.5 MB (10.05%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -3,23 +3,23 @@ public:
     int ans = 0;
     int d;
     void dfs(int node, vector<int>& edges, unordered_map<int,int>& pathvis, 
-    unordered_set<int>& vis){
-        vis.insert(node);
+    vector<int>& vis){
+        vis[node] = 1;
         pathvis[node] = d;
         int nxt = edges[node];
         if (pathvis.find(nxt) != pathvis.end()){
             ans = max(ans, d - pathvis[nxt] + 1);
             return;
         }
-        if (nxt == -1 || vis.find(nxt) != vis.end()) return;
+        if (nxt == -1 || vis[nxt]) return;
         d++;
         dfs(nxt, edges, pathvis, vis);
     }
     int longestCycle(vector<int>& edges) {
         int n = edges.size();
-        unordered_set<int> vis;
+        vector<int> vis(n,0);
         for (int i = 0; i < n; i++){
-            if (vis.find(i) == vis.end()){
+            if (!vis[i]){
                 unordered_map<int,int> pathvis;
                 d = 1;
                 dfs(i, edges, pathvis, vis);
