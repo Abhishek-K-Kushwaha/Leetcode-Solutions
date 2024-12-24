5e3fa2a198b95fa49848d0c835bc37d1cb1eb44e
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-24 13:18:32 +0530

    Time: 11 ms (23.86%), Space: 22.9 MB (18.56%) - LeetHub

--- 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.cpp
+++ 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.cpp
@@ -6,42 +6,47 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     int i = 0;
-    int dfs(string& s, int lvl, TreeNode* par, bool isright){
+    int dfs(string& s, int lvl, TreeNode* par, bool isright) {
         string temp;
-        while(i < s.size() && s[i] != '-'){
+        while (i < s.size() && s[i] != '-') {
             temp.push_back(s[i]);
             i++;
         }
         int n = stoi(temp);
         TreeNode* node = new TreeNode(n);
-        if (!isright) par->left = node;
-        else par->right = node;
-        if (i >= s.size()) return -1;
+        if (!isright)
+            par->left = node;
+        else
+            par->right = node;
+        if (i >= s.size())
+            return -1;
         int cnt = 0;
-        while (s[i]=='-'){
+        while (s[i] == '-') {
             cnt++;
             i++;
         }
-        if (cnt == lvl){
+        if (cnt == lvl) {
             cnt = dfs(s, lvl, par, 1);
         }
-        if (cnt < lvl) return cnt;
-        cnt = dfs(s, lvl+1, node, 0);
-        if (cnt == lvl){
+        if (cnt < lvl)
+            return cnt;
+        cnt = dfs(s, lvl + 1, node, 0);
+        if (cnt == lvl) {
             return dfs(s, lvl, par, 1);
-        }
-        else return cnt;
+        } else
+            return cnt;
     }
 
     TreeNode* recoverFromPreorder(string s) {
         string temp;
-        while(i < s.size() && s[i] != '-'){
+        while (i < s.size() && s[i] != '-') {
             temp.push_back(s[i]);
             i++;
         }
