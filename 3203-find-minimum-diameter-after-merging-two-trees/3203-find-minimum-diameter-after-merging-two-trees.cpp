d25fdab589821f878a581fbf002e6e34a125cf73
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-24 11:15:48 +0530

    Time: 246 ms (91.9%), Space: 298.9 MB (75.39%) - LeetHub

---
+++ 3203-find-minimum-diameter-after-merging-two-trees/3203-find-minimum-diameter-after-merging-two-trees.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int dia = 0;
+    int d = 0;
+    int dfs(int node, int par, vector<vector<int>>& adj){
+        //int n = adj[node].size();
+        //vector<int> dis(adj[node].size()-1,0);
+        int maxd = 0;
+        for (int nxt:adj[node]){
+            if (nxt != par){
+                d++;
+                int temp = dfs(nxt, node, adj);
+                d--;
+                dia = max(dia, maxd-d + temp-d);
+                maxd = max(maxd, temp);
+            }
+        }
+        //sort(dis.rbegin(), dis.rend());
+        //if (n >= 2) dia = max(dia, dis[0]+dis[1]-d);
+        dia = max(dia, d);
+        //d--;
+        return max(maxd, d);
+    }
+    int minimumDiameterAfterMerge(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
+        int n = edges1.size(), m = edges2.size();
+        vector<vector<int>> adj1(n+1);
+        for (auto& it: edges1){
+            adj1[it[0]].push_back(it[1]);
+            adj1[it[1]].push_back(it[0]);
+        }
+        dfs(0, -1, adj1);
+        int dia1 = dia;
+        //cout << dia << endl;
+        vector<vector<int>> adj2(m+1);
+        for (auto& it: edges2){
+            adj2[it[0]].push_back(it[1]);
+            adj2[it[1]].push_back(it[0]);
+        }
+        d = 0;
+        dia = 0;
+        dfs(0, -1, adj2);
+        int dia2 = dia;
+        int ans = (dia1+1)/2 + (dia2+1)/2 + 1;
+        ans = max(ans, dia1);
+        ans = max(ans, dia2);
+        return ans;
+    }
+};
