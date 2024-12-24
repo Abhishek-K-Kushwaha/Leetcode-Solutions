840067a7420f56d8c2a3695e03adaf7f3c44af00
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-24 11:18:27 +0530

    Time: 273 ms (73.21%), Space: 299.1 MB (72.9%) - LeetHub

--- 3203-find-minimum-diameter-after-merging-two-trees/3203-find-minimum-diameter-after-merging-two-trees.cpp
+++ 3203-find-minimum-diameter-after-merging-two-trees/3203-find-minimum-diameter-after-merging-two-trees.cpp
@@ -20,15 +20,15 @@ public:
         int n = edges1.size(), m = edges2.size();
         vector<vector<int>> adj1(n+1);
         for (auto& it: edges1){
-            adj1[it[0]].push_back(it[1]);
-            adj1[it[1]].push_back(it[0]);
+            adj1[it[0]].emplace_back(it[1]);
+            adj1[it[1]].emplace_back(it[0]);
         }
         dfs(0, -1, adj1);
         int dia1 = dia;
         vector<vector<int>> adj2(m+1);
         for (auto& it: edges2){
-            adj2[it[0]].push_back(it[1]);
-            adj2[it[1]].push_back(it[0]);
+            adj2[it[0]].emplace_back(it[1]);
+            adj2[it[1]].emplace_back(it[0]);
         }
         d = 0;
         dia = 0;
