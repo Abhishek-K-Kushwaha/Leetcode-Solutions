97e87a4a205d651685086cf0e9330aef4affb170
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-26 13:11:56 +0530

    Time: 16 ms (57.01%), Space: 13.9 MB (57.6%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,19 +2,21 @@ class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        vector<int> prev(2001,0), curr(2001,0);
-        prev[1000]=1;
+        int tot = accumulate(nums.begin(), nums.end(),0);
+        if (target < -tot || target > tot) return 0;
+        vector<int> prev(2*tot+1,0), curr(2*tot+1,0);
+        prev[tot]=1;
         for (int i = n-1; i >= 0; i--){
-            for (int t = 0; t < 2001; t++){
+            for (int t = 0; t < 2*tot+1; t++){
                 //curr[t]=0;
-                if (t-1000+nums[i] <= 1000) 
+                if (t-tot+nums[i] <= tot) 
                     curr[t] += prev[t+nums[i]];
-                if (t-1000-nums[i] >= -1000) 
+                if (t-tot-nums[i] >= -tot) 
                     curr[t] += prev[t-nums[i]]; 
             }
             prev = curr;
-            curr.assign(2001,0);
+            curr.assign(2*tot+1,0);
         }
-        return prev[target+1000];
+        return prev[target+tot];
     }
 };
