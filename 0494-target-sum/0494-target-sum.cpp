0275ce2b10f64ff70cbbad6ebb19c3a884cb2340
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-26 13:30:10 +0530

    Time: 7 ms (71.87%), Space: 15.1 MB (47.4%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -11,17 +11,21 @@ public:
             return 0;
         int d = (sum - target) / 2;
         if (d < 0) return 0;
-        vector<vector<int>> dp(n + 1, vector<int>(d + 1, 0));
-        dp[0][0] = 1;
+        //vector<vector<int>> dp(n + 1, vector<int>(d + 1, 0));
+        vector<int> prev(d+1,0);
+        //dp[0][0] = 1;
+        prev[0] = 1;
         for (int i = 1; i <= n; i++) {
+            vector<int> curr(d+1);
             for (int t = 0; t <= d; t++) {
-                int notpick = dp[i - 1][t];
+                int notpick = prev[t];
                 int pick = 0;
                 if (nums[i - 1] <= t)
-                    pick = dp[i - 1][t - nums[i - 1]];
-                dp[i][t] = pick + notpick;
+                    pick = prev[t - nums[i - 1]];
+                curr[t] = pick + notpick;
             }
+            prev = curr;
         }
-        return dp[n][d];
+        return prev[d];
     }
 };
