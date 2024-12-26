668ad62a78d5d6d5b012767cb340270cc4335b69
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-26 11:59:41 +0530

    Time: 47 ms (49.01%), Space: 42.7 MB (20.37%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int sum = 0;
-        int n = 0;
-        for (int num : nums) {
-            sum += num;
-            n++;
-        }
-        if ((sum - target) % 2 != 0)
-            return 0;
-        int d = (sum - target) / 2;
-        if (d < 0) return 0;
-        vector<vector<int>> dp(n + 1, vector<int>(d + 1, 0));
-        dp[0][0] = 1;
-        for (int i = 1; i <= n; i++) {
-            for (int t = 0; t <= d; t++) {
-                int notpick = dp[i - 1][t];
-                int pick = 0;
-                if (nums[i - 1] <= t)
-                    pick = dp[i - 1][t - nums[i - 1]];
-                dp[i][t] = pick + notpick;
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int> (2001, 0));
+        dp[n][1000]=1;
+        //int ways;
+        for (int i = n-1; i >= 0; i--){
+            for (int t = 0; t < 2001; t++){
+                if (t-1000+nums[i] <= 1000 && t-1000+nums[i] >= -1000) 
+                    dp[i][t] += dp[i+1][t+nums[i]];
+                if (t-1000-nums[i] <= 1000 && t-1000-nums[i] >= -1000) 
+                    dp[i][t] += dp[i+1][t-nums[i]]; 
+                //if (dp[i][t])cout << dp[i][t] << " "<< i<<" "<< t-1000<< endl;
             }
         }
-        return dp[n][d];
+        return dp[0][target+1000];
     }
 };
