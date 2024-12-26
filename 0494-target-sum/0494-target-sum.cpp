ae4b7d695957f2310d5021246c203381ccfa255e
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-26 13:12:13 +0530

    Time: 19 ms (54.77%), Space: 13.6 MB (59.34%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -4,18 +4,18 @@ public:
         int n = nums.size();
         int tot = accumulate(nums.begin(), nums.end(),0);
         if (target < -tot || target > tot) return 0;
-        vector<int> prev(2*tot+1,0), curr(2*tot+1,0);
+        vector<int> prev(2*tot+1,0), curr(2*tot+1);
         prev[tot]=1;
         for (int i = n-1; i >= 0; i--){
             for (int t = 0; t < 2*tot+1; t++){
-                //curr[t]=0;
+                curr[t]=0;
                 if (t-tot+nums[i] <= tot) 
                     curr[t] += prev[t+nums[i]];
                 if (t-tot-nums[i] >= -tot) 
                     curr[t] += prev[t-nums[i]]; 
             }
             prev = curr;
-            curr.assign(2*tot+1,0);
+            //curr.assign(2*tot+1,0);
         }
         return prev[target+tot];
     }
