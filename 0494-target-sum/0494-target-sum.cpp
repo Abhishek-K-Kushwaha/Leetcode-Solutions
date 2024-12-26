969ffccaffd6a44f923d6b47d027ecee696599fa
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-26 12:04:40 +0530

    Time: 38 ms (49.61%), Space: 14.7 MB (48.73%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,18 +2,21 @@ class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int> (2001, 0));
-        dp[n][1000]=1;
-        //int ways;
+        //vector<vector<int>> dp(n+1, vector<int> (2001, 0));
+        vector<int> prev(2001,0), curr(2001);
+        //dp[n][1000]=1;
+        prev[1000]=1;
         for (int i = n-1; i >= 0; i--){
             for (int t = 0; t < 2001; t++){
+                curr[t]=0;
                 if (t-1000+nums[i] <= 1000) 
-                    dp[i][t] += dp[i+1][t+nums[i]];
+                    curr[t] += prev[t+nums[i]];
                 if (t-1000-nums[i] >= -1000) 
-                    dp[i][t] += dp[i+1][t-nums[i]]; 
+                    curr[t] += prev[t-nums[i]]; 
                 //if (dp[i][t])cout << dp[i][t] << " "<< i<<" "<< t-1000<< endl;
             }
+            prev = curr;
         }
-        return dp[0][target+1000];
+        return curr[target+1000];
     }
 };
