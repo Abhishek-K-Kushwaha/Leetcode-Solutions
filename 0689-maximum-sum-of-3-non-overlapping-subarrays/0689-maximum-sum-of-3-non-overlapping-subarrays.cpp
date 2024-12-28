48578d4ac7b011ceb957d2aa0e926069cbfdcc84
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-28 22:37:06 +0530

    Time: 2 ms (65.58%), Space: 26.7 MB (54.47%) - LeetHub

---
+++ 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> maxSumOfThreeSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> winsum(n+1-k);
+        winsum[0] = accumulate(nums.begin(), nums.begin()+k,0);
+        for (int i = k; i < n; i++){
+            winsum[i-k+1] = winsum[i-k]+nums[i]-nums[i-k];
+        }
+        vector<int> lmax(n);
+        int maxsum = 0;
+        for (int i = k-1; i <= n-k; i++){
+            if (winsum[i-k+1] > maxsum){
+                lmax[i] = i - k + 1;
+                maxsum = winsum[i-k+1];
+            }
+            else{
+                lmax[i] = lmax[i-1];
+            }
+        }
+        // for (auto it: lmax){
+        //     cout << it << endl;
+        // }
+        vector<int> rmax(n);
+        maxsum = 0;
+        for (int i = n-k; i >= k; i--){
+            if (winsum[i] >= maxsum){
+                rmax[i] = i;
+                maxsum = winsum[i];
+            }
+            else{
+                rmax[i] = rmax[i+1];
+            }
+        }
+        int l, r;
+        vector<int> ans(3);
+        maxsum = 0;
+        for (int i = k; i <= n-2*k; i++){
+            l = lmax[i-1];
+            r = rmax[i+k];
+            if (winsum[l]+winsum[r]+winsum[i] > maxsum){
+                ans = {l,i,r};
+                maxsum = winsum[l]+winsum[r]+winsum[i];
+            }
+        }
+        return ans;
+    }
+};
