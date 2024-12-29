ec1d3075be5492ca5bcff3b6cf6d966595e0d325
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-29 15:05:58 +0530

    Time: 59 ms (46.73%), Space: 51.1 MB (16.32%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
-    int subarraySum(vector<int>& arr, int K) {
-        int N = arr.size();
-        unordered_map<int, int> dict;
-        int count = 0;
-        int pre_sum = 0;
-        for (int i = 0; i < N; i++) {
-            pre_sum += arr[i];
-            if (pre_sum == K) {
-                count++;
-            }
-            if (dict.find(pre_sum - K) != dict.end()) {
-                count += dict[pre_sum - K];
-            }
-            // if (dict.find(pre_sum) == dict.end())
-            //     dict[pre_sum] = 1;
-            // else
-            //     dict[pre_sum]++;
-            dict[pre_sum]++;
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> presum;
+        int ans = 0;
+        int sum = 0;
+        for (int i:nums){
+            sum += i;
+            if (sum == k) ans++;
+            ans += presum[sum-k];
+            presum[sum]++; 
         }
-        return count;
+        return ans;
     }
 };
