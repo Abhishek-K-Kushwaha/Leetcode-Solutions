4016e918a0cf44c82c644eaf18f544c987ab49d0
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-29 14:37:27 +0530

    Time: 55 ms (81.02%), Space: 32.7 MB (93.19%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -6,25 +6,30 @@ public:
         int tlen = target.size();
         if (tlen > len) return 0;
         int MOD = 1e9 + 7;
-        vector<vector<int>> dp(tlen + 1, vector<int>(len + 1, 0));
-        for (int j = 0; j <= len; j++) {
-            dp[tlen][j] = 1;
-        }
+        // vector<vector<int>> dp(tlen + 1, vector<int>(len + 1, 0));
+        // for (int j = 0; j <= len; j++) {
+        //     dp[tlen][j] = 1;
+        // }
         vector<vector<int>> freq(len, vector<int>(26, 0));
         for (int j = 0; j < len; j++) {
             for (int k = 0; k < n; k++) {
                 freq[j][words[k][j] - 'a']++;
             }
         }
+        vector<int> prev(len + 1, 1);
+        vector<int> curr(len + 1);
+        curr[len] = 0;
         long long val;
         for (int i = tlen - 1; i >= 0; i--) {
+            //curr[len] = 0;
             for (int j = len - 1; j >= 0; j--) {
-                val = dp[i][j + 1];
-                val += (long long)freq[j][target[i] - 'a'] * dp[i + 1][j + 1];
+                val = curr[j + 1];
+                val += (long long)freq[j][target[i] - 'a'] * prev[j + 1];
                 val %= MOD;
-                dp[i][j] = val;
+                curr[j] = val;
             }
+            prev = curr;
         }
-        return dp[0][0];
+        return prev[0];
     }
 };
