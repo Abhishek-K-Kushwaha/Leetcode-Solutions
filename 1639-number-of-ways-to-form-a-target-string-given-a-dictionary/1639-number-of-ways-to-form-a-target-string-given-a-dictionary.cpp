6076ac7ef7b575b0278b842d02abb7efe53d0c95
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-29 14:28:19 +0530

    Time: 67 ms (74.45%), Space: 58.9 MB (42.82%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -5,40 +5,25 @@ public:
         int len = words[0].size();
         int tlen = target.size();
         int MOD = 1e9 + 7;
-        vector<vector<int>> dp(tlen+1, vector<int> (len+1,0));
-        for (int j = 0; j <= len; j++){
+        vector<vector<int>> dp(tlen + 1, vector<int>(len + 1, 0));
+        for (int j = 0; j <= len; j++) {
             dp[tlen][j] = 1;
         }
-        vector<vector<int>> freq(len, vector<int> (26,0));
-        for (int j = 0; j < len; j++){
-            for (int k = 0; k < n; k++){
-                freq[j][words[k][j]-'a']++;
+        vector<vector<int>> freq(len, vector<int>(26, 0));
+        for (int j = 0; j < len; j++) {
+            for (int k = 0; k < n; k++) {
+                freq[j][words[k][j] - 'a']++;
             }
         }
         long long val;
-        for (int i = tlen-1; i >= 0; i--){
-            for (int j = len-1; j >= 0; j--){
-                val = dp[i][j+1];
-                val += (long long) freq[j][target[i]-'a']*dp[i+1][j+1];
-                // for (int k = 0; k < n; k++){
-                //     if (words[k][j] == target[i]){ 
-                //         val += dp[i+1][j+1];
-                //         //if (val > INT_MAX) cout << val << " ";
-                //         val %= MOD;
-                //     }
-                // }
-                //cout<< endl;
-                //cout<< i << " " << j <<" "<< val << endl;
+        for (int i = tlen - 1; i >= 0; i--) {
+            for (int j = len - 1; j >= 0; j--) {
+                val = dp[i][j + 1];
+                val += (long long)freq[j][target[i] - 'a'] * dp[i + 1][j + 1];
                 val %= MOD;
                 dp[i][j] = val;
             }
         }
-        // for (auto& it:dp){
-        //     for (int i:it){
-        //         cout<< i << " "; 
-        //     }
-        //     cout << endl;
-        // }
         return dp[0][0];
     }
 };
