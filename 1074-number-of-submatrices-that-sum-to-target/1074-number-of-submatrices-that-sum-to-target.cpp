a9f034ada150c9d8bb9ad43ae0d603529b6bc316
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-30 16:34:33 +0530

    Time: 377 ms (88.37%), Space: 98.4 MB (81.16%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -10,15 +10,15 @@ public:
         }
         int ans = 0;
         int sum;
+        unordered_map<int,int> dict;
         for (int l = 0; l < n; l++){
             for (int r = l; r < n; r++){
-                unordered_map<int,int> dict;
-                dict[0] = 1;
+                dict = {{0,1}};
                 sum = 0;
                 for (int i= 0; i < m; i++){
                     sum += matrix[i][r];
                     if (l > 0) sum -= matrix[i][l-1];
-                    ans += dict[sum-target];
+                    if (dict.find(sum-target) != dict.end()) ans += dict[sum-target];
                     dict[sum]++;
                 }
             }
