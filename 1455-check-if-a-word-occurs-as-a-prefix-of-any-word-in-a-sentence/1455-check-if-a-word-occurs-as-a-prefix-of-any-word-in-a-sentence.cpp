2be2646f6353d83e23822eedd9d0b2dcfce085d7
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-31 13:08:29 +0530

    Time: 0 ms (100%), Space: 8.1 MB (97.49%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -4,23 +4,22 @@ public:
         int i = 0, ind = 1;
         int len = searchWord.size();
         bool flag = true;
-        while (i < sentence.size()){
-            int j = 0;
+        while (i < sentence.size()) {
+            int j;
             flag = true;
-            for (j = 0; j < len; j++){
-                if (sentence[i+j] != searchWord[j]){
+            for (j = 0; j < len; j++) {
+                if (sentence[i + j] != searchWord[j]) {
                     flag = false;
                     break;
                 }
             }
-            if (flag) return ind;
+            if (flag)
+                return ind;
             i += j;
-            while (sentence[i] != ' ' && i < sentence.size()){
+            while (sentence[i] != ' ' && i < sentence.size()) {
                 i++;
-                
             }
             i++;
-            //cout<< "Next word at: "<< i << " "<< sentence[i] <<" \n";
             ind++;
         }
         return -1;
