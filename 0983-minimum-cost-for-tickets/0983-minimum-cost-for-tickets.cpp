f951833643de8ad0c6b1a5d64fb81daee7938823
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-12-31 11:41:17 +0530

    Time: 1 ms (36.2%), Space: 13.4 MB (32.35%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int f(int i, vector<int>& days, vector<int>& costs){
+    int f(int i, vector<int>& days, vector<int>& costs) {
         auto it = lower_bound(days.begin(), days.end(), i);
         int ind = it - days.begin();
-        if (ind >= days.size()) return 0;
-        else return ind;
+        if (ind >= days.size())
+            return 0;
+        else
+            return ind;
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
-        int maxday = days[n-1];
-        vector<int> dp(n+1);
+        int maxday = days[n - 1];
+        vector<int> dp(n + 1);
         int d1, d7, d30;
-        for (int i = n-1; i >= 0; i--){
-            d1 = costs[0] + dp[f(days[i]+1, days, costs)];
-            d7 = costs[1] + dp[f(days[i]+7, days, costs)];
-            d30 = costs[2] + dp[f(days[i]+30, days, costs)];
+        for (int i = n - 1; i >= 0; i--) {
+            d1 = costs[0] + dp[f(days[i] + 1, days, costs)];
+            d7 = costs[1] + dp[f(days[i] + 7, days, costs)];
+            d30 = costs[2] + dp[f(days[i] + 30, days, costs)];
             dp[i] = min(d1, min(d7, d30));
         }
         return dp[0];
