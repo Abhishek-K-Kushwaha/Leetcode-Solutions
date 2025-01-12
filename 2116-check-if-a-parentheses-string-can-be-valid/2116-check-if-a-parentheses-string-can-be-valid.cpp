13acdccb3aa267d87c076966f9db27e821ac2d6c
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2025-01-12 22:45:40 +0530

    Time: 16 ms (44.16%), Space: 34.7 MB (16.75%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
@@ -4,29 +4,26 @@ public:
         stack<int> open;
         stack<int> both;
         int n = s.size();
-        if (n % 2) return false;
-        for (int i = 0; i < n; i++){
-            if (locked[i] == '0'){
+        if (n % 2)
+            return false;
+        for (int i = 0; i < n; i++) {
+            if (locked[i] == '0') {
                 both.push(i);
-            }
-            else if (s[i] == '('){
+            } else if (s[i] == '(') {
                 open.push(i);
-            }
-            else{
-                if (!open.empty()){
+            } else if (!open.empty()) {
                     open.pop();
-                }
-                else if (!both.empty()){
-                    both.pop();
-                }
-                else return false;
-            }
+            } else if (!both.empty()) {
+                both.pop();
+            } else
+                return false;
         }
-        while (!both.empty() && !open.empty() && open.top() < both.top()){
+        while (!both.empty() && !open.empty() && open.top() < both.top()) {
             both.pop();
             open.pop();
         }
-        if (!open.empty()) return false;
+        if (!open.empty())
+            return false;
         return true;
     }
 };
