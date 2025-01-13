ea4e61fd9cccdea9889d9e69e9abc462cdca6036
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2025-01-13 10:22:35 +0530

    Time: 11 ms (77.02%), Space: 32.1 MB (74.19%) - LeetHub

---
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        vector<int> freq(26,0);
+        for (auto& c:s){
+            freq[c-'a']++;
+        }
+        int ans = 0;
+        for (int i:freq){
+            if (i%2) ans+=1;
+            else if (i) ans+=2;
+        }
+        return ans;
+    }
+};
