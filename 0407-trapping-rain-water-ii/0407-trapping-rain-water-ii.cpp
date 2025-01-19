9edd6f7b56f293bb69f0fc54552ef28a850392b1
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2025-01-19 19:29:26 +0530

    Time: 31 ms (65.7%), Space: 27.1 MB (33.2%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.cpp
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.cpp
@@ -3,36 +3,35 @@ public:
     int trapRainWater(vector<vector<int>>& hmap) {
         int m = hmap.size();
         int n = hmap[0].size();
-        vector<vector<int>> vis(m, vector<int> (n));
-        set<pair<int,int>> st;  
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
-                if (i == 0 || j == 0 || i == m-1 || j == n-1){
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        set<pair<int, int>> st;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0 || i == m - 1 || j == n - 1) {
                     vis[i][j] = 1;
-                    st.insert({hmap[i][j], i*n + j});
+                    st.insert({hmap[i][j], i * n + j});
                 }
-                else vis[i][j] = 0;
             }
         }
         int node, h, r, c, nc, nr, ans = 0;
-        vector<int> dr = {1,-1,0,0}, dc = {0,0,1,-1};
-        while (!st.empty()){
+        vector<int> dr = {1, -1, 0, 0}, dc = {0, 0, 1, -1};
+        while (!st.empty()) {
             auto it = st.begin();
             h = (*it).first;
             node = (*it).second;
             st.erase(it);
             r = node / n;
             c = node % n;
-            for (int i = 0; i < 4; i++){
+            for (int i = 0; i < 4; i++) {
                 nr = r + dr[i];
                 nc = c + dc[i];
-                if (nr > 0 && nr < m && nc > 0 && nc < n && !vis[nr][nc]){
+                if (nr > 0 && nr < m && nc > 0 && nc < n && !vis[nr][nc]) {
                     vis[nr][nc] = 1;
-                    if (hmap[nr][nc] < h){ 
+                    if (hmap[nr][nc] < h) {
                         ans += (h - hmap[nr][nc]);
-                        st.insert({h, nr*n + nc});
-                    }
-                    else st.insert({hmap[nr][nc], nr*n + nc});
+                        st.insert({h, nr * n + nc});
+                    } else
+                        st.insert({hmap[nr][nc], nr * n + nc});
                 }
             }
         }
