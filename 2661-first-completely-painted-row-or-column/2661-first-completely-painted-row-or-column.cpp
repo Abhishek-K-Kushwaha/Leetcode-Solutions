27e468b1eb419dd0f20f9ebf73412237ee46d5ac
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2025-01-20 10:47:48 +0530

    Time: 8 ms (87.2%), Space: 132.1 MB (96.34%) - LeetHub

--- 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.cpp
+++ 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.cpp
@@ -3,17 +3,19 @@ public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
-        vector<int> loc(m*n+1);
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
-                loc[mat[i][j]] = i*n + j;
+        vector<int> loc(m * n + 1);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                loc[mat[i][j]] = i * n + j;
             }
         }
         vector<int> rcnt(m, 0), ccnt(n, 0);
-        for (int i = 0; i < arr.size(); i++){
+        for (int i = 0; i < arr.size(); i++) {
             auto it = loc[arr[i]];
-            rcnt[it / n]++; ccnt[it % n]++;
-            if (rcnt[it / n] == n || ccnt[it % n] == m) return i; 
+            rcnt[it / n]++;
+            ccnt[it % n]++;
+            if (rcnt[it / n] == n || ccnt[it % n] == m)
+                return i;
         }
         return -1;
     }
