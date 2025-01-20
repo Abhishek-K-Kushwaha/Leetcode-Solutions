fd8194d0abd76e8638149996401ff02ef36a5b8e
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2025-01-20 10:40:21 +0530

    Time: 11 ms (84.76%), Space: 132.2 MB (96.04%) - LeetHub

--- 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.cpp
+++ 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.cpp
@@ -3,17 +3,17 @@ public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
-        vector<pair<int,int>> loc(m*n+1);
+        vector<int> loc(m*n+1);
         for (int i = 0; i < m; i++){
             for (int j = 0; j < n; j++){
-                loc[mat[i][j]] = {i,j};
+                loc[mat[i][j]] = i*n + j;
             }
         }
         vector<int> rcnt(m, 0), ccnt(n, 0);
         for (int i = 0; i < arr.size(); i++){
             auto it = loc[arr[i]];
-            rcnt[it.first]++; ccnt[it.second]++;
-            if (rcnt[it.first] == n || ccnt[it.second] == m) return i; 
+            rcnt[it / n]++; ccnt[it % n]++;
+            if (rcnt[it / n] == n || ccnt[it % n] == m) return i; 
         }
         return -1;
     }
