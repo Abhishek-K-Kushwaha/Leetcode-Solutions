1e7c3071b1a0ceae2decb2d5bc9c13a84de540ca
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2025-01-20 10:50:20 +0530

    Time: 99 ms (56.71%), Space: 161.7 MB (68.6%) - LeetHub

--- 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.cpp
+++ 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.cpp
@@ -1,22 +1,42 @@
 class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
-        int m = mat.size();
-        int n = mat[0].size();
-        vector<int> loc(m * n + 1);
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                loc[mat[i][j]] = i * n + j;
-            }
-        }
-        vector<int> rcnt(m, 0), ccnt(n, 0);
+        // Map to store the index of each number in the arr
+        unordered_map<int, int> numToIndex;
         for (int i = 0; i < arr.size(); i++) {
-            auto it = loc[arr[i]];
-            rcnt[it / n]++;
-            ccnt[it % n]++;
-            if (rcnt[it / n] == n || ccnt[it % n] == m)
-                return i;
+            numToIndex[arr[i]] = i;
         }
-        return -1;
+
+        int result = INT_MAX;
+        int numRows = mat.size();
+        int numCols = mat[0].size();
+
+        // Check for the earliest row to be completely painted
+        for (int row = 0; row < numRows; row++) {
+            // Tracks the greatest index in this column
+            int lastElementIndex = INT_MIN;
+            for (int col = 0; col < numCols; col++) {
+                int indexVal = numToIndex[mat[row][col]];
+                lastElementIndex = max(lastElementIndex, indexVal);
+            }
+            // Update result with the minimum index where this row is fully
+            // painted
+            result = min(result, lastElementIndex);
+        }
+
+        // Check for the earliest column to be completely painted
+        for (int col = 0; col < numCols; col++) {
+            // Tracks the greatest index in this column
+            int lastElementIndex = INT_MIN;
+            for (int row = 0; row < numRows; row++) {
+                int indexVal = numToIndex[mat[row][col]];
+                lastElementIndex = max(lastElementIndex, indexVal);
+            }
+            // Update result with the minimum index where this column is fully
+            // painted
+            result = min(result, lastElementIndex);
+        }
+
+        return result;
     }
 };
