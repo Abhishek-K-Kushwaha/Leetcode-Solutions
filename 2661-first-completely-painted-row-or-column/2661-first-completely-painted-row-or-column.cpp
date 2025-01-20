47588931a474d356cb10a79d737912cbeda0129b
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2025-01-20 10:53:04 +0530

    Time: 11 ms (84.76%), Space: 131.2 MB (98.17%) - LeetHub

--- 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.cpp
+++ 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.cpp
@@ -1,42 +1,29 @@
 class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
-        // Map to store the index of each number in the arr
         int numRows = mat.size();
         int numCols = mat[0].size();
-        vector<int> numToIndex(numRows*numCols+1);
+        vector<int> numToIndex(numRows * numCols + 1);
         for (int i = 0; i < arr.size(); i++) {
             numToIndex[arr[i]] = i;
         }
-
         int result = INT_MAX;
-
-        // Check for the earliest row to be completely painted
         for (int row = 0; row < numRows; row++) {
-            // Tracks the greatest index in this column
             int lastElementIndex = INT_MIN;
             for (int col = 0; col < numCols; col++) {
                 int indexVal = numToIndex[mat[row][col]];
                 lastElementIndex = max(lastElementIndex, indexVal);
             }
-            // Update result with the minimum index where this row is fully
-            // painted
             result = min(result, lastElementIndex);
         }
-
-        // Check for the earliest column to be completely painted
         for (int col = 0; col < numCols; col++) {
-            // Tracks the greatest index in this column
             int lastElementIndex = INT_MIN;
             for (int row = 0; row < numRows; row++) {
                 int indexVal = numToIndex[mat[row][col]];
                 lastElementIndex = max(lastElementIndex, indexVal);
             }
-            // Update result with the minimum index where this column is fully
-            // painted
             result = min(result, lastElementIndex);
         }
-
         return result;
     }
 };
