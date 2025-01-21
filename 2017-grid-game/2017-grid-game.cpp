b8c12fdb4bbc99c0836efd8618f04135d451c0de
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2025-01-21 14:06:43 +0530

    Time: 3 ms (59.28%), Space: 79.5 MB (16.74%) - LeetHub

---
+++ 2017-grid-game/2017-grid-game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long gridGame(vector<vector<int>>& grid) {
+        int n = grid[0].size();
+        if (n == 1) return 0;
+        vector<vector<long long>> presum(2, vector<long long> (n));
+        presum[1][0] = grid[1][0]; presum[0][n-1] = grid[0][n-1];
+        for (int i = 1; i < n; i++){
+            presum[1][i] = (long long)grid[1][i] + presum[1][i-1];
+            presum[0][n-i-1] = (long long)grid[0][n-i-1] + presum[0][n-i];
+        }
+        long long p;
+        long long ans = min(presum[0][1], presum[1][n-2]);
+        for (int i = 1; i < n-1; i++){
+            p = max(presum[0][i+1], presum[1][i-1]);
+            ans = min(ans, p);
+        }
+        return ans;
+    }
+};
